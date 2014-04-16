1b3785e23453a8bf115d99e574f89299012c2f17
Alastair Munro <alastair.munro@totaralms.com>
2014-04-16 12:50:08 +0100

    T-12160 rol: Fix breadcrumbs when viewing staffs record of learning
    Change-Id: I3cfa4e0cd0ed76f5b189a28c87454dfbb695aa4f
    Reviewed-on: https://review.totaralms.com/4839
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: David Curry <david.curry@totaralms.com>
    Reviewed-by: Yuliya Bozhko <yuliya.bozhko@totaralms.com>

--- totara/plan/record/competencies.php
+++ totara/plan/record/competencies.php
@@ -95,7 +95,7 @@ $report->include_js();
 ///
 
 $PAGE->navbar->add(get_string('mylearning', 'totara_core'), new moodle_url('/my/'));
-$PAGE->navbar->add($strheading, new moodle_url('/totara/plan/record/index.php'));
+$PAGE->navbar->add($strheading, new moodle_url('/totara/plan/record/index.php', array('userid' => $userid)));
 $PAGE->navbar->add($strsubheading);
 
 $PAGE->set_title($strheading);
--- totara/plan/record/programs_recurring.php
+++ totara/plan/record/programs_recurring.php
@@ -106,7 +106,7 @@ $report->include_js();
 /// Display the page
 ///
 $PAGE->navbar->add(get_string('mylearning', 'totara_core'), new moodle_url('/my/'));
-$PAGE->navbar->add($strheading, new moodle_url('/totara/plan/record/index.php'));
+$PAGE->navbar->add($strheading, new moodle_url('/totara/plan/record/index.php', array('userid' => $userid)));
 $PAGE->navbar->add($strsubheading);
 $PAGE->set_title($strheading);
 $PAGE->set_button($report->edit_button());
