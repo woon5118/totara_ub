bd522bcb108cae78255cb609436396915c6f19f6
Brian Barnes <brian.barnes@totaralms.com>
2014-05-07 16:00:05 +1200

    T-12238 alerts: The alerts block is now a list instead of a table
    Change-Id: I1f671ccf3bc85d456f447f03413912b8fe35e4ff
    Reviewed-on: https://review.totaralms.com/4957
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Rob Tyler <rob.tyler@totaralms.com>
    Reviewed-by: Alastair Munro <alastair.munro@totaralms.com>

--- blocks/totara_alerts/block_totara_alerts.php
+++ blocks/totara_alerts/block_totara_alerts.php
@@ -73,82 +73,9 @@ class block_totara_alerts extends block_base {
             return $this->content;
         }
 
-        // Now build the table of results.
-        $table = new html_table();
-        $table->attributes['class'] = 'fullwidth invisiblepadded';
-        if (!empty($this->msgs)) {
-            $cnt = 0;
-            foreach ($this->msgs as $msg) {
-                // Status Icon.
-                $cnt++;
+        $renderer = $this->page->get_renderer('block_totara_alerts');
+        $this->content->text = $renderer->display_alerts($this->msgs, $total);
 
-                $cssclass = totara_message_cssclass($msg->msgtype);
-                $rowbkgd = ($cnt % 2) ? 'shade' : 'noshade';
-                $msglink = !empty($msg->contexturl) ? $msg->contexturl : '';
-                // Build the array of 3 table cell objects.
-                $cells = array();
-
-                $icon = $OUTPUT->pix_icon('msgicons/' . $msg->icon, format_string($msg->subject), 'totara_core', array('class' => "msgicon {$cssclass}",  'alt'=>format_string($msg->subject)));
-                if (!empty($msglink)) {
-                    $url = new moodle_url($msglink);
-                    $attributes = array('href' => $url);
-                    $cellcontent = html_writer::tag('a', $icon, $attributes);
-                } else {
-                    $cellcontent = $icon;
-                }
-                $cell = new html_table_cell($cellcontent);
-                $cell->attributes['class'] = 'status';
-                $cells[] = $cell;
-
-                $text = format_string($msg->subject ? $msg->subject : $msg->fullmessage);
-                if (!empty($msglink)) {
-                    $url = new moodle_url($msglink);
-                    $attributes = array('href' => $url);
-                    $cellcontent = html_writer::tag('a', $text, $attributes);
-                } else {
-                    $cellcontent = $text;
-                }
-                $cell = new html_table_cell($cellcontent);
-                $cell->attributes['class'] = 'statement';
-                $cells[] = $cell;
-
-                $moreinfotext = get_string('clickformoreinfo', 'block_totara_alerts');
-                $icon = $OUTPUT->pix_icon('i/info', $moreinfotext, 'moodle', array('class'=>'msgicon', 'title' => $moreinfotext, 'alt' => $moreinfotext));
-                $detailjs = totara_message_alert_popup($msg->id, null, 'detailalert');
-                $url = new moodle_url($msglink);
-                $attributes = array('href' => $url, 'id' => "detailalert{$msg->id}-dialog");
-                $cellcontent = html_writer::tag('a', $icon, $attributes) . $detailjs;
-                $cell = new html_table_cell($cellcontent);
-                $cell->attributes['class'] = 'action';
-                $cells[] = $cell;
-                $row = new html_table_row($cells);
-                $row->attributes['class'] = $rowbkgd;
-                $table->data[] = $row;
-            }
-        }
-
-        $this->content->text = '';
-        $count = count($this->msgs);
-        if ($count) {
-            $this->content->text .= html_writer::tag('p', get_string('showingxofx', 'block_totara_alerts', array('count' => $count, 'total' => $total)));
-        } else {
-            if (!empty($CFG->block_totara_alerts_showempty)) {
-                if (!empty($this->config->showempty)) {
-                    $this->content->text .= html_writer::tag('p', get_string('noalerts', 'block_totara_alerts'));
-                } else {
-                    return '';
-                }
-            } else {
-                return '';
-            }
-        }
-
-        $this->content->text .= html_writer::table($table);
-        if (!empty($this->msgs)) {
-            $url = new moodle_url('/totara/message/alerts.php', array('sesskey' => sesskey()));
-            $link = html_writer::link($url, get_string('viewallnot', 'block_totara_alerts'));
-            $this->content->footer = html_writer::tag('div', $link, array('class' => 'viewall'));
-        }
         return $this->content;
     }
 }
