18b3e16902ff5164e133c01f888e6320ec645204
Petr Skoda <petr.skoda@totaralms.com>
2014-06-24 16:01:04 +1200

    T-11447 upgrade: make sure frontpage redirects to admin/ before upgrade from Moodle
    Change-Id: I76b2c94abe639feacb49cd48f49c4749c9ebf788
    Reviewed-on: https://review.totaralms.com/5214
    Reviewed-by: Alastair Munro <alastair.munro@totaralms.com>
    Reviewed-by: Ciaran Irvine <ciaran.irvine@totaralms.com>
    Tested-by: Jenkins Automation <jenkins@totaralms.com>

--- lib/setuplib.php
+++ lib/setuplib.php
@@ -1268,7 +1268,7 @@ function redirect_if_major_upgrade_required() {
     global $CFG;
     $lastmajordbchanges = 2013100400.02;
     if (empty($CFG->version) or (float)$CFG->version < $lastmajordbchanges or
-            during_initial_install() or !empty($CFG->adminsetuppending)) {
+            during_initial_install() or !empty($CFG->adminsetuppending) or !isset($CFG->totara_version)) {
         try {
             @\core\session\manager::terminate_current();
         } catch (Exception $e) {
