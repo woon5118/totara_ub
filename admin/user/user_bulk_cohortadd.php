d3013118c5c23d1dab575081f9784da8921053ed
Maria Torres <maria.torres@totaralms.com>
2014-06-25 17:10:18 +1200

    T-12488 audience: Remove dynamic audiences from the 'add to audiences' list in bulk user actions
    Change-Id: I0656ba8db8555cb13db056767ecf4c2a32cde570
    Reviewed-on: https://review.totaralms.com/5221
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Brian Barnes <brian.barnes@totaralms.com>
    Reviewed-by: David Curry <david.curry@totaralms.com>

--- admin/user/user_bulk_cohortadd.php
+++ admin/user/user_bulk_cohortadd.php
@@ -39,7 +39,7 @@ $users = $SESSION->bulk_users;
 $strnever = get_string('never');
 
 $cohorts = array(''=>get_string('choosedots'));
-$allcohorts = $DB->get_records('cohort');
+$allcohorts = $DB->get_records('cohort', array('cohorttype' => cohort::TYPE_STATIC));
 foreach ($allcohorts as $c) {
     if (!empty($c->component)) {
         // external cohorts can not be modified
