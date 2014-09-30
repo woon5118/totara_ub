f4220296160fa827ec0509d44cd82a756da85810
Brian Barnes <brian.barnes@totaralms.com>
2014-10-01 12:04:53 +1300

    T-13118 theme: removed dock for IE9 and older
    Change-Id: I7a3654f0fd8f9a7e41505caa0c4503f1f477a6e9
    Reviewed-on: https://review.totaralms.com/6007
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: David Curry <david.curry@totaralms.com>
    Reviewed-by: Nathan Lewis <nathan.lewis@totaralms.com>

--- theme/standardtotararesponsive/config.php
+++ theme/standardtotararesponsive/config.php
@@ -78,5 +78,7 @@ $THEME->layouts = array(
     ),
 );
 
-$THEME->enable_dock = true;
+if (!(core_useragent::is_ie() && !core_useragent::check_ie_version('10.0'))) {
+    $THEME->enable_dock = true;
+}
 $THEME->rendererfactory = 'theme_overridden_renderer_factory';
