fcf7e823229b093b0d9e655b1f9aeeb62696f031
Alastair Munro <alastair.munro@totaralms.com>
2014-10-09 11:55:41 +0100

    T-13326 login: Remove redundant add_to_log call
    Change-Id: I39482da1c0904df8a3b380f8ebdf75aff73379de
    Reviewed-on: https://review.totaralms.com/6117
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Simon Coggins <simon.coggins@totaralms.com>
    Reviewed-by: Petr Skoda <petr.skoda@totaralms.com>

--- login/index.php
+++ login/index.php
@@ -200,15 +200,12 @@ if ($frm and isset($frm->username)) {                             // Login WITH 
             die;
         }
 
-    /// Let's get them all set up.
-        add_to_log(SITEID, 'user', 'login', "view.php?id=$USER->id&course=".SITEID,
-                   $user->id, 0, $user->id);
-
         // if multiple logins not permitted, clear out any existing sessions for this user
         if (!empty($CFG->preventmultiplelogins)) {
             \core\session\manager::kill_user_sessions($user->id);
         }
 
+        /// Let's get them all set up.
         complete_user_login($user);
 
         // sets the username cookie
