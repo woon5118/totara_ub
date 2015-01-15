54812a4d96f7eb1d39775967d89289d0ddfbab10
Eugene Venter <eugene@catalyst.net.nz>
2015-01-15 17:25:34 +1300

    TL-6023: Add program completion block
    Thanks to Eugene from Catalyst IT for contributing the patch.
    
    Change-Id: I7b444645cdd33510e221baefc1a2d245f7b767ed
    Reviewed-on: https://review.totaralms.com/7726
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Brian Barnes <brian.barnes@totaralms.com>
    Reviewed-by: Sam Hemelryk <sam.hemelryk@totaralms.com>

--- lib/classes/plugin_manager.php
+++ lib/classes/plugin_manager.php
@@ -986,7 +986,7 @@ class core_plugin_manager {
                 ,'totara_addtoplan', 'totara_alerts',
                 'totara_my_learning_nav', 'totara_my_team_nav', 'totara_quicklinks',
                 'totara_recent_learning', 'totara_report_graph', 'totara_report_manager', 'totara_stats',
-                'totara_tasks', 'facetoface', 'totara_certifications', 'gaccess',
+                'totara_tasks', 'facetoface', 'totara_certifications', 'gaccess', 'totara_program_completion',
                 'totara_dashboard',
             ),
 
