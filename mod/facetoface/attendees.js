1333c05ed70cf838a983ae33ac514e0c917d6d97
Sam Hemelryk <sam.hemelryk@totaralms.com>
2015-02-12 17:09:51 +1300

    T-14017 behat: fixed broken tests in 2.7
    Change-Id: Ie799278399a2272c15eaf6e78b236f92ad447268
    Reviewed-on: https://review.totaralms.com/7068
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Petr Skoda <petr.skoda@totaralms.com>
    Reviewed-by: Sam Hemelryk <sam.hemelryk@totaralms.com>

--- mod/facetoface/attendees.js
+++ mod/facetoface/attendees.js
@@ -585,6 +585,8 @@ M.totara_f2f_attendees = M.totara_f2f_attendees || {
             });
         }
 
+        var lastactionvalue = null;
+
         // Handle actions drop down.
         $(document).on('change', 'select#menuf2f-actions', function() {
             var select = $(this);
@@ -597,8 +599,17 @@ M.totara_f2f_attendees = M.totara_f2f_attendees || {
 
             // Get current value
             var current = select.val();
-
-            // Reset to default
+            // If its the empty or if its the same as the last action.
+            if (current === '' || current === lastactionvalue) {
+                // No need to do anything here we're returning to the default value.
+                lastactionvalue = null;
+                return;
+            }
+            // Record the current selection as the last value. The next line triggers a change again and some browsers
+            // we not have an updated empty value when this happen.
+            lastactionvalue = current;
+            // Reset to default.
+            // This triggers the change event for a second time but we catch it with the above check.
             select.val(0);
 
             // Do an action dependant on what value was chosen
