bcdefe4c952b06fff11d320a5a587858e4dd3090
Valerii Kuznetsov <valerii.kuznetsov@totaralms.com>
2015-03-02 11:47:28 +1300

    T-14101 facetoface: Fix column names for case sensitive collations
    Change-Id: Ia1dde760bb6cd7932a12411e5702d9cff61a9ead
    Reviewed-on: https://review.totaralms.com/7236
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: David Curry <david.curry@totaralms.com>
    Reviewed-by: Nathan Lewis <nathan.lewis@totaralms.com>

--- enrol/totara_facetoface/lib.php
+++ enrol/totara_facetoface/lib.php
@@ -203,7 +203,7 @@ class enrol_totara_facetoface_plugin extends enrol_plugin {
             $f2fids[$session->facetoface] = $session->facetoface;
         }
         list($idin, $params) = $DB->get_in_or_equal($f2fids);
-        $facetofaces = $DB->get_records_select('facetoface', "ID $idin", $params);
+        $facetofaces = $DB->get_records_select('facetoface', "id $idin", $params);
 
         $selectpositiononsignupglobal = get_config(null, 'facetoface_selectpositiononsignupglobal');
         if ($selectpositiononsignupglobal) {
