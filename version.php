7e6996eae88403d4dcfc405a63a964573bae216d
Ciaran Irvine <ciaran.irvine@totaralms.com>
2015-03-18 15:01:00 +1300

    T-7189 version.php: Tagging version 2.7.1
    Change-Id: Ia42f10a01ac7c720bd03e25702499d89a55d1f5d
    Reviewed-on: https://review.totaralms.com/7372
    Reviewed-by: David Curry <david.curry@totaralms.com>
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Scott Blair <scott.blair@totaralms.com>

--- version.php
+++ version.php
@@ -46,6 +46,6 @@ $maturity = MATURITY_STABLE;             // this version's maturity level
 
 $TOTARA = new stdClass();
 
-$TOTARA->version    = '2.7.0+';       // Please keep as string.
-$TOTARA->build      = '20150302.00';  // Please keep as string.
+$TOTARA->version    = '2.7.1';        // Please keep as string.
+$TOTARA->build      = '20150318.00';  // Please keep as string.
 $TOTARA->release    = "{$TOTARA->version} (Build: {$TOTARA->build})";
