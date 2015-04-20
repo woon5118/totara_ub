2db5a0bd76fa4e03413ef93b4b1a7a71805bab7a
Nathan Lewis <nathan.lewis@totaralms.com>
2015-04-21 11:56:55 +1200

    TL-5311 reportbuilder: Add course completion history report source
    Change-Id: I1df4e99de378a4abd316906187149e6b75fb75f7
    Reviewed-on: https://review.totaralms.com/7447
    Reviewed-by: Oleg Demeshev <oleg.demeshev@totaralms.com>
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: David Curry <david.curry@totaralms.com>

--- totara/plan/rb_sources/lang/en/rb_source_dp_course_completion_history.php
+++ totara/plan/rb_sources/lang/en/rb_source_dp_course_completion_history.php
@@ -17,15 +17,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @package totara
- * @subpackage reportbuilder
+ * @package totara_reportbuilder
  */
 
-// lang file for totara/plan/rb_sources/rb_source_dp_course_completion_history.php
-$string['sourcetitle'] = 'Record of Learning: Previous Course Completions';
-
-$string['timecompleted'] = 'Date last completed';
 $string['grade'] = 'Grade at time of completion';
-
-// column types for this source, as strings
-$string['type_base'] = 'Previous Course Completions';
+$string['sourcetitle'] = 'Record of Learning: Previous Course Completions';
+$string['timecompleted'] = 'Date last completed';
+$string['type_base'] = 'Course Completion History';
