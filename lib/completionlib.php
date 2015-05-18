19b612de66a158defd1f4646ad25913a30e05333
Nathan Lewis <nathan.lewis@totaralms.com>
2015-05-19 09:43:51 +1200

    TL-6558 coursecompletion: Fixed another query to prevent use of IN
    Change-Id: If1b967a12b7e2bb14045fdc74d49f48302cf591b
    Reviewed-on: https://review.totaralms.com/7857
    Reviewed-by: Alastair Munro <alastair.munro@totaralms.com>
    Reviewed-by: Maria Torres <maria.torres@totaralms.com>
    Tested-by: Jenkins Automation <jenkins@totaralms.com>

--- lib/completionlib.php
+++ lib/completionlib.php
@@ -984,28 +984,44 @@ class completion_info {
 
         $transaction = $DB->start_delegated_transaction();
 
-        // Find all RPL completions for this course.
-        $sql = "SELECT userid FROM {course_completions}
-                 WHERE course = ? AND status = ?";
-        if ($rpl = $DB->get_fieldset_sql($sql, array($this->course_id, COMPLETION_STATUS_COMPLETEVIARPL))) {
-            list($insql, $inparams) = $DB->get_in_or_equal($rpl, SQL_PARAMS_QM, '', false);
-            $insql = ' AND userid ' . $insql;
-        } else {
-            $insql = '';
-            $inparams = array();
-        }
-        $params = array_merge(array($this->course_id), $inparams);
-        $DB->delete_records_select('course_completions', "course = ? {$insql}", $params);
-        // Do not remove RPL activity completion records.
-        $DB->delete_records_select('course_completion_crit_compl', "course = ? AND (rpl = '' OR rpl IS NULL) {$insql}", $params);
+        $params = array('courseid' => $this->course_id,
+            'eventstarted' => STATS_EVENT_COURSE_STARTED,
+            'eventcomplete' => STATS_EVENT_COURSE_COMPLETE,
+            'status' => COMPLETION_STATUS_COMPLETEVIARPL);
 
-        // Remove stats data.
-        $params = array_merge(array(STATS_EVENT_COURSE_STARTED, STATS_EVENT_COURSE_COMPLETE), $params);
-        $DB->delete_records_select(
-            'block_totara_stats',
-            "eventtype IN (?, ?) AND data2 = ? {$insql}",
-            $params
-        );
+        $criteriasql =
+            "DELETE
+               FROM {course_completion_crit_compl}
+              WHERE course = :courseid
+                AND (rpl = '' OR rpl IS NULL)
+                AND NOT EXISTS (SELECT 1
+                                  FROM {course_completions} cc
+                                 WHERE cc.userid = {course_completion_crit_compl}.userid
+                                   AND cc.course = {course_completion_crit_compl}.course
+                                   AND cc.status = :status)";
+        // Do not remove RPL activity completion records.
+        $DB->execute($criteriasql, $params);
+
+        $statssql =
+            "DELETE
+               FROM {block_totara_stats}
+              WHERE data2 = :courseid
+                AND eventtype IN (:eventstarted, :eventcomplete)
+                AND NOT EXISTS (SELECT 1
+                                  FROM {course_completions} cc
+                                 WHERE cc.userid = {block_totara_stats}.userid
+                                   AND cc.course = {block_totara_stats}.data2
+                                   AND cc.status = :status)";
+        $DB->execute($statssql, $params);
+
+        // Do course_completions last, as the other two may depend on these records.
+        $coursesql =
+            "DELETE
+               FROM {course_completions}
+              WHERE course = :courseid
+                AND status <> :status";
+        $DB->execute($coursesql, $params);
+
         $transaction->allow_commit();
     }
 
