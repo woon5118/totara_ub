47e74cb288ab103f3d5d8fac431da8f22ad8fb8d
Alastair Munro <alastair.munro@totaralms.com>
2015-06-22 10:55:27 +0100

    TL-6745 totara/hierarchy: Fix permissions checks for appraisal goal review question
    Change permission check to allow a manager's manager and appraiser to be
    able to view a learner's goals so they are able review them.
    
    Change-Id: I6eb11870947e0a65d242868e791408b335ea2dd1
    Reviewed-on: https://review.totaralms.com/8201
    Reviewed-by: David Curry <david.curry@totaralms.com>
    Reviewed-by: Oleg Demeshev <oleg.demeshev@totaralms.com>
    Tested-by: Jenkins Automation <jenkins@totaralms.com>

--- totara/hierarchy/prefix/goal/lib.php
+++ totara/hierarchy/prefix/goal/lib.php
@@ -1295,6 +1295,10 @@ class goal extends hierarchy {
             $context = context_user::instance($userid);
             $syscontext = context_system::instance();
 
+            // Get team leader and appraiser for permissions checks.
+            $teamleader = totara_get_teamleader($userid);
+            $appraiser = totara_get_appraiser($userid);
+
             // Set up permissions checks so we don't have to do them everytime.
             if (has_capability('totara/hierarchy:managegoalassignments', $syscontext)) {
                 // Admin permissions, can do anything with this one permission.
@@ -1308,8 +1312,10 @@ class goal extends hierarchy {
                     GOAL_ASSIGNMENT_MANAGER => true,
                     GOAL_ASSIGNMENT_ADMIN => true,
                 );
-            } else if (totara_is_manager($userid)) {
-                // Manager permissions.
+            } else if (totara_is_manager($userid) ||
+                (!empty($teamleader) && $USER->id == $teamleader->id) ||
+                (!empty($appraiser) && $USER->id == $appraiser->id)) {
+                // Manager, manager's manager and appraiser permissions.
                 $permissions['can_view_personal'] = has_capability('totara/hierarchy:viewstaffpersonalgoal', $context);
                 $permissions['can_edit_personal'] = has_capability('totara/hierarchy:managestaffpersonalgoal', $context);
                 $permissions['can_view_company'] = has_capability('totara/hierarchy:viewstaffcompanygoal', $context);
