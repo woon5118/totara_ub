a5b6806ffeaeaf9d7fea0dd4340db350e2332df4
Petr Skoda <petr.skoda@totaralms.com>
2015-06-29 13:46:56 +1200

    TL-6838 switch back to Totara 2.9dev
    Change-Id: I056949dfc61297221c31108b582a6563c226b6a5
    Reviewed-on: https://review.totaralms.com/8296
    Reviewed-by: Petr Skoda <petr.skoda@totaralms.com>
    Tested-by: Petr Skoda <petr.skoda@totaralms.com>

--- version.php
+++ version.php
@@ -46,6 +46,6 @@ $maturity = MATURITY_ALPHA;             // this version's maturity level
 
 $TOTARA = new stdClass();
 
-$TOTARA->version    = '8.0dev';       // Please keep as string.
+$TOTARA->version    = '2.9dev';       // Please keep as string.
 $TOTARA->build      = '20150519.00';  // Please keep as string.
 $TOTARA->release    = "{$TOTARA->version} (Build: {$TOTARA->build})";
