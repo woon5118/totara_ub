d4e01b52892f2bfdaa94a189d863988df42b869a
Petr Skoda <petr.skoda@totaralms.com>
2015-08-18 18:10:21 +1200

    Bump build to match stables
    Change-Id: I19c4845cffd897eb7d8fa95cee6102bba1683473

--- version.php
+++ version.php
@@ -47,5 +47,5 @@ $maturity = MATURITY_ALPHA;             // this version's maturity level
 $TOTARA = new stdClass();
 
 $TOTARA->version    = '2.9dev';       // Please keep as string.
-$TOTARA->build      = '20150729.00';  // Please keep as string.
+$TOTARA->build      = '20150818.00';  // Please keep as string.
 $TOTARA->release    = "{$TOTARA->version} (Build: {$TOTARA->build})";
