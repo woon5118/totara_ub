9cb68285931ba01cf6bed21aae04af59de1b5058
Murali Nair <murali.nair@totaralms.com>
2015-09-03 09:28:18 +1200

    TL-4527 core: modified utils.php to remove undeclared variables
    Change-Id: Ia8ed6004eb566f5eed7e0d094c7da90904cec63e
    Reviewed-on: https://review.totaralms.com/8914
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Sam Hemelryk <sam.hemelryk@totaralms.com>
    Reviewed-by: Alastair Munro <alastair.munro@totaralms.com>

--- totara/core/utils.php
+++ totara/core/utils.php
@@ -87,13 +87,14 @@ function sql_sequence($field, $items, $type=SQL_PARAMS_QM, $negate = false) {
     }
 
     $not = $negate ? 'NOT' : '';
-    if ($DB->get_dbfamily() != 'oracle' || $count($items) <= 1000) {
+    if ($DB->get_dbfamily() != 'oracle' || count($items) <= 1000) {
         list($sql, $params) = $DB->get_in_or_equal($items, $type, 'param', !$negate);
 
         return array(" $field " . $sql, $params);
     }
 
     $out = array();
+    $outparams = array();
     while ($some_items = totara_pop_n($items, 1000)) {
         list($sql, $params) = $DB->get_in_or_equal($items, $type, 'param', !$negate);
         $out[] =" $field " . $sql;
