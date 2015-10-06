a3cf717160c22b1f9ec265273512e70ee720d164
Alastair Munro <alastair.munro@totaralms.com>
2015-10-06 17:19:45 +0100

    TL-7571 enrol/totara_facetoface: Make validation less confusing when no session is selected
    Change-Id: I0b5aa593a8938194e618d8a5270783cfbdbeb85d
    Reviewed-on: https://review.totaralms.com/9371
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Brian Barnes <brian.barnes@totaralms.com>
    Reviewed-by: Simon Player <simon.player@totaralms.com>

--- enrol/totara_facetoface/signup_form.php
+++ enrol/totara_facetoface/signup_form.php
@@ -132,11 +132,9 @@ class enrol_totara_facetoface_signup_form extends moodleform {
                 $mform->addElement('html', html_writer::end_div());
             }
             $mform->addElement('html', html_writer::end_div());
-
-            if ($sessionsavailable) {
-                $mform->addRule('sid', null, 'required', null, 'client');
-            }
         }
+
+        $mform->addElement('static', 'sessionrequired', '');
 
         if ($sessionsavailable) {
             $notificationdisabled = get_config(null, 'facetoface_notificationdisable');
@@ -284,6 +282,11 @@ class enrol_totara_facetoface_signup_form extends moodleform {
     public function validation($data, $files) {
         $errors = parent::validation($data, $files);
         $mform = $this->_form;
+
+        // Fail validation if a session is not selected.
+        if (!isset($data['sid'])) {
+            $errors['sessionrequired'] = get_string('mustselectsession', 'enrol_totara_facetoface');
+        }
 
         if (!empty($data['sid'])) {
             $elementid = 'selfapprovaltc' . $data['sid'];
