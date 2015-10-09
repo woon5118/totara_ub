d13d4b5fc668952a4b6e65c1052f057433fc54af
Oleg Demeshev <oleg.demeshev@totaralms.com>
2015-10-09 15:12:38 +1300

    TL-7570 hierarchy: Fix UI Admin navigation block access for user with right capabilities to view Positions and Organisations Hierarchy
    Change-Id: I65d131bbc0c617225cad0afe43aa0c63eb53c4f9
    Reviewed-on: https://review.totaralms.com/9342
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Petr Skoda <petr.skoda@totaralms.com>
    Reviewed-by: Alastair Munro <alastair.munro@totaralms.com>

--- totara/hierarchy/settings.php
+++ totara/hierarchy/settings.php
@@ -31,7 +31,8 @@
         $ADMIN->add('positions', new admin_externalpage('positionmanage', get_string('positionmanage', 'totara_hierarchy'),
             "{$CFG->wwwroot}/totara/hierarchy/framework/index.php?prefix=position",
             array('totara/hierarchy:createpositionframeworks', 'totara/hierarchy:updatepositionframeworks', 'totara/hierarchy:deletepositionframeworks',
-                'totara/hierarchy:createposition', 'totara/hierarchy:updateposition', 'totara/hierarchy:deleteposition')));
+                'totara/hierarchy:createposition', 'totara/hierarchy:updateposition', 'totara/hierarchy:deleteposition',
+                'totara/hierarchy:viewpositionframeworks')));
 
         $ADMIN->add('positions', new admin_externalpage('positiontypemanage', get_string('managepositiontypes', 'totara_hierarchy'),
             "{$CFG->wwwroot}/totara/hierarchy/type/index.php?prefix=position",
@@ -48,7 +49,8 @@
     $ADMIN->add('organisations', new admin_externalpage('organisationmanage', get_string('organisationmanage', 'totara_hierarchy'),
             "{$CFG->wwwroot}/totara/hierarchy/framework/index.php?prefix=organisation",
             array('totara/hierarchy:createorganisationframeworks', 'totara/hierarchy:updateorganisationframeworks', 'totara/hierarchy:deleteorganisationframeworks',
-                  'totara/hierarchy:createorganisation', 'totara/hierarchy:updateorganisation', 'totara/hierarchy:deleteorganisation')));
+                  'totara/hierarchy:createorganisation', 'totara/hierarchy:updateorganisation', 'totara/hierarchy:deleteorganisation',
+                  'totara/hierarchy:vieworganisationframeworks')));
 
     $ADMIN->add('organisations', new admin_externalpage('organisationtypemanage', get_string('manageorganisationtypes', 'totara_hierarchy'),
             "{$CFG->wwwroot}/totara/hierarchy/type/index.php?prefix=organisation",
