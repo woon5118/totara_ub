99ebe3218346a6ea0b22dcf40b4328a29df997ef
Brian Barnes <brian.barnes@totaralms.com>
2015-11-09 10:48:02 +1300

    TL-7875 groups: replaced HTML table behind groupings
    Change-Id: Ic6194b5418480f29de88a1418c5153587c0b3928
    Reviewed-on: https://review.totaralms.com/9760
    Reviewed-by: Sam Hemelryk <sam.hemelryk@totaralms.com>
    Reviewed-by: Simon Player <simon.player@totaralms.com>
    Tested-by: Jenkins Automation <jenkins@totaralms.com>

--- group/assign.php
+++ group/assign.php
@@ -137,46 +137,36 @@ echo $OUTPUT->header();
 <div id="addmembersform">
     <h3 class="main"><?php print_string('addgroupstogroupings', 'group'); echo ": $groupingname"; ?></h3>
     <form id="assignform" method="post" action="">
-    <div>
     <input type="hidden" name="sesskey" value="<?php p(sesskey()); ?>" />
-    <table summary="" class="generaltable generalbox groupmanagementtable boxaligncenter">
-    <tr>
-      <td id="existingcell">
+    <div class="row-fluid user-multiselect">
+        <div class="span5">
           <label for="removeselect"><?php print_string('existingmembers', 'group', $currentmemberscount); ?></label>
-          <div class="userselector" id="removeselect_wrapper">
           <select name="removeselect[]" size="20" id="removeselect" multiple="multiple"
                   onfocus="document.getElementById('assignform').add.disabled=true;
                            document.getElementById('assignform').remove.disabled=false;
                            document.getElementById('assignform').addselect.selectedIndex=-1;">
           <?php echo $currentmembersoptions ?>
-          </select></div></td>
-      <td id="buttonscell">
-        <p class="arrow_button">
+          </select>
+        </div>
+        <div class="span2 controls">
             <input name="add" id="add" type="submit"
                    value="<?php echo $OUTPUT->larrow().'&nbsp;'.get_string('add'); ?>"
-                   title="<?php print_string('add'); ?>" /><br>
+                   title="<?php print_string('add'); ?>" />
             <input name="remove" id="remove" type="submit"
                    value="<?php echo get_string('remove').'&nbsp;'.$OUTPUT->rarrow(); ?>"
                    title="<?php print_string('remove'); ?>" />
-        </p>
-      </td>
-      <td id="potentialcell">
+        </div>
+        <div class="span5">
           <label for="addselect"><?php print_string('potentialmembers', 'group', $potentialmemberscount); ?></label>
-          <div class="userselector" id="addselect_wrapper">
           <select name="addselect[]" size="20" id="addselect" multiple="multiple"
                   onfocus="document.getElementById('assignform').add.disabled=false;
                            document.getElementById('assignform').remove.disabled=true;
                            document.getElementById('assignform').removeselect.selectedIndex=-1;">
-         <?php echo $potentialmembersoptions ?>
-         </select>
-          </div>
-       </td>
-    </tr>
-    <tr><td colspan="3" id="backcell">
-        <input type="submit" name="cancel" value="<?php print_string('backtogroupings', 'group'); ?>" />
-    </td></tr>
-    </table>
+          <?php echo $potentialmembersoptions ?>
+          </select>
+        </div>
     </div>
+    <input type="submit" name="cancel" value="<?php print_string('backtogroupings', 'group'); ?>" />
     </form>
 </div>
 
