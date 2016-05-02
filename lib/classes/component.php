01833efdcce596b15390995a4ff5b8aeb44dafdc
Petr Skoda <petr.skoda@totaralms.com>
2016-05-02 15:01:19 +1200

    TL-8958 classloader: add lookup via namespace across plugins
    Change-Id: Id82605420e34021e51fc8bfa5d226bdb33a2cf5a
    Reviewed-on: https://review.totaralms.com/11302
    Reviewed-by: Sam Hemelryk <sam.hemelryk@totaralms.com>
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Murali Nair <murali.nair@totaralms.com>

--- lib/classes/component.php
+++ lib/classes/component.php
@@ -887,6 +887,77 @@ $cache = '.var_export($cache, true).';
     }
 
     /**
+     * Returns classes from given namespace across all Totara plugins and core.
+     *
+     * @param string $namespace plugin namespace, this cannot include the name of the plugin, for example 'rb\display'
+     * @param string $instanceof full name of class or interface that returned classes should be extending/implementing
+     * @param string $component restrict results to one plugin only, for example 'totara_reportbuilder'
+     * @param bool $excludeabstract exclude abstract classes
+     * @return string[] list of full class names in given namespace
+     */
+    public static function get_namespace_classes($namespace, $instanceof = null, $component = null, $excludeabstract = true) {
+        self::init();
+
+        $interface = null;
+        if ($instanceof) {
+            $instanceof = ltrim($instanceof, '\\'); // Normalise the class/interface name.
+            if (class_exists($instanceof, true)) {
+                $interface = false;
+            } else if (interface_exists($instanceof, true)) {
+                $interface = true;
+            } else {
+                debugging('Invalid $instanceof parameter, it must be a name of class or interface: ' . $instanceof, DEBUG_DEVELOPER);
+                return array();
+            }
+        }
+
+        if ($component) {
+            $match = '/^' . preg_quote($component) .'\\\\' . preg_quote($namespace) . '\\\\[^\\\\]+$/';
+            $quickmatch = $component . '\\' . $namespace . '\\';
+        } else {
+            $match = '/^[^\\\\]+\\\\' . preg_quote($namespace) . '\\\\[^\\\\]+$/';
+            $quickmatch = '\\' . $namespace . '\\';
+        }
+
+        $classes = array();
+        foreach (self::$classmap as $class => $unused) {
+            if (strpos($class, $quickmatch) === false) {
+                // There are very many classes in Totara, this should be faster than regrex
+                // because only a small portion of classes is returned here.
+                continue;
+            }
+            if (!preg_match($match, $class)) {
+                continue;
+            }
+            if (!class_exists($class, true)) {
+                // Most likely an interface.
+                continue;
+            }
+            $rc = new ReflectionClass($class);
+            if ($excludeabstract and $rc->isAbstract()) {
+                // This is intended to exclude base classes and helpers.
+                continue;
+            }
+            if ($instanceof) {
+                if ($instanceof === $class) {
+                    // Exact match is fine, base classes are filtered out by $excludeabstract.
+                } else if ($interface) {
+                    if (!$rc->implementsInterface($instanceof)) {
+                        continue;
+                    }
+                } else {
+                    if (!$rc->isSubclassOf($instanceof)) {
+                        continue;
+                    }
+                }
+            }
+            $classes[] = $class;
+        }
+
+        return $classes;
+    }
+
+    /**
      * Get a list of all the plugins of a given type that contain a particular file.
      *
      * @param string $plugintype the type of plugin, e.g. 'mod' or 'report'.
