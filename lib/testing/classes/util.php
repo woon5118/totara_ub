f888cdacedb478b3d9e3014ee95f8ce12538bd0c
Sam Hemelryk <sam.hemelryk@totaralearning.com>
2016-07-15 13:29:58 +1200

    TL-9642 / MDL-54573 testing: don't uppercase dbtype
    Change-Id: I37530cac92dc01573139b981f42180167f4d38ec

--- lib/testing/classes/util.php
+++ lib/testing/classes/util.php
@@ -1022,10 +1022,10 @@ abstract class testing_util {
         $env['phpversion'] = $phpversion;
 
         // Add database type and version.
-        $dbtype = $DB->get_dbvendor();
+        $dbtype = $CFG->dbtype;
         $dbinfo = $DB->get_server_info();
         $dbversion = $dbinfo['version'];
-        $env['dbtype'] = $dbtype; // Totara: No case magic here!!!
+        $env['dbtype'] = $dbtype;
         $env['dbversion'] = $dbversion;
 
         // OS details.
