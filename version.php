97bacf9a0f2a50c230458a97451d42a2c12d78c7
Petr Skoda <petr.skoda@totaralearning.com>
2016-10-12 09:21:29 +1300

    Bump up daily RC build to force cache purging
    Change-Id: I8174dd83b9512cc81a7aabd7c6ec588f63ec3722
    Reviewed-on: https://review.totaralms.com/13132
    Reviewed-by: Sam Hemelryk <sam.hemelryk@totaralearning.com>
    Tested-by: Sam Hemelryk <sam.hemelryk@totaralearning.com>

--- version.php
+++ version.php
@@ -47,5 +47,5 @@ $maturity = MATURITY_RC;                // this version's maturity level
 $TOTARA = new stdClass();
 
 $TOTARA->version    = '9.0rc1+';      // Please keep as string.
-$TOTARA->build      = '20160922.00';  // Please keep as string.
+$TOTARA->build      = '20161012.00';  // Please keep as string.
 $TOTARA->release    = "{$TOTARA->version} (Build: {$TOTARA->build})";
