62cccf94f9146cf31c3bbaace8fd573bb13a13cd
Petr Skoda <petr.skoda@totaralearning.com>
2016-12-29 13:20:09 +1300

    TL-12321 Fix Evergreen migration from Moodle 3.0.7
    We need to keep moodle plugin versions in sync
    even if we revert commits such as in TL-11367.
    
    Change-Id: I296f20df918a5aac7bc2c493264c85873096bda7
    Reviewed-on: https://review.totaralms.com/14154
    Tested-by: Petr Skoda <petr.skoda@totaralearning.com>
    Reviewed-by: Sam Hemelryk <sam.hemelryk@totaralearning.com>

--- lib/setup.php
+++ lib/setup.php
@@ -385,8 +385,8 @@ if (!defined('MOODLE_INTERNAL')) { // Necessary because cli installer has to def
 
 // Totara: we support migration from this particular Moodle release only.
 if (!defined('MOODLE_MIGRATION_VERSION')) {
-    define('MOODLE_MIGRATION_VERSION', '2015111606.00'); // Keep as string to simplify comparison with DB data.
-    define('MOODLE_MIGRATION_RELEASE', '3.0.6 (Build: 20160912)');
+    define('MOODLE_MIGRATION_VERSION', '2015111607.00'); // Keep as string to simplify comparison with DB data.
+    define('MOODLE_MIGRATION_RELEASE', '3.0.7 (Build: 20161114)');
 }
 
 // core_component can be used in any scripts, it does not need anything else.
