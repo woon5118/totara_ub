54cc28038b78aea3d37b559040ebbe28c177afb8
Murali Nair <murali.nair@totaralearning.com>
2017-01-04 10:11:53 +1300

    TL-12301 mod_facetoface: recreate course links in notifications when a seminar is restored
    Change-Id: I121e39d6ca203d850b61bc733fbeb5b0839700f1
    Reviewed-on: https://review.totaralms.com/14163
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Rob Tyler <rob.tyler@totaralearning.com>
    Reviewed-by: Valerii Kuznetsov <valerii.kuznetsov@totaralearning.com>

--- mod/facetoface/backup/moodle2/restore_facetoface_activity_task.class.php
+++ mod/facetoface/backup/moodle2/restore_facetoface_activity_task.class.php
@@ -53,6 +53,7 @@ class restore_facetoface_activity_task extends restore_activity_task {
         $contents = array();
 
         $contents[] = new restore_decode_content('facetoface', array('intro'), 'facetoface');
+        $contents[] = new restore_decode_content('facetoface_notification', array('body', 'managerprefix'));
 
         return $contents;
     }
