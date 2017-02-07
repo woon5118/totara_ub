f1221b230a225138e872b9b823395ade88909600
Brendan Heywood <brendan@catalyst-au.net>
2017-02-08 10:07:36 +1300

    TL-11635 / MDL-53357 admin: Don't break urls after saving admin settings

--- admin/settings.php
+++ admin/settings.php
@@ -40,7 +40,7 @@ $errormsg  = '';
 
 if ($data = data_submitted() and confirm_sesskey()) {
     if (admin_write_settings($data)) {
-        $statusmsg = get_string('changessaved');
+        redirect($PAGE->url, get_string('changessaved'), null, \core\output\notification::NOTIFY_SUCCESS);
     }
 
     if (empty($adminroot->errors)) {
@@ -48,6 +48,7 @@ if ($data = data_submitted() and confirm_sesskey()) {
             case 'site': redirect("$CFG->wwwroot/");
             case 'admin': redirect("$CFG->wwwroot/$CFG->admin/");
         }
+        redirect($PAGE->url);
     } else {
         $errormsg = get_string('errorwithsettings', 'admin');
         $firsterror = reset($adminroot->errors);
@@ -88,7 +89,7 @@ if (empty($SITE->fullname)) {
 
     // ---------------------------------------------------------------------------------------------------------------
 
-    echo '<form action="settings.php" method="post" id="adminsettings">';
+    echo '<form action="' . $PAGE->url . '" method="post" id="adminsettings">';
     echo '<div class="settingsform clearfix">';
     echo html_writer::input_hidden_params($PAGE->url);
     echo '<input type="hidden" name="sesskey" value="'.sesskey().'" />';
@@ -132,7 +133,7 @@ if (empty($SITE->fullname)) {
 
     // ---------------------------------------------------------------------------------------------------------------
 
-    echo '<form action="settings.php" method="post" id="adminsettings">';
+    echo '<form action="' . $PAGE->url . '" method="post" id="adminsettings">';
     echo '<div class="settingsform clearfix">';
     echo html_writer::input_hidden_params($PAGE->url);
     echo '<input type="hidden" name="sesskey" value="'.sesskey().'" />';
