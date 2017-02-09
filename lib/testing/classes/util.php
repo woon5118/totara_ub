7f7e9e7f11e68b282867e1914cfb2e3ac4de91e4
Rajesh Taneja <rajesh@moodle.com>
2017-02-09 14:47:04 +1300

    TL-12190 / MDL-55764 behat: Reset static update db list

--- lib/testing/classes/util.php
+++ lib/testing/classes/util.php
@@ -959,6 +959,9 @@ abstract class testing_util {
         if (file_exists($tablesupdatedfile)) {
             unlink($tablesupdatedfile);
         }
+
+        // Reset static cache of cli process.
+        self::reset_updated_table_list();
     }
 
     /**
