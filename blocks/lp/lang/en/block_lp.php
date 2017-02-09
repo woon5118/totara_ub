f263af40aaa8de4542a34ee59ff30ce1aef2cf04
Issam Taboubi <issam.taboubi@umontreal.ca>
2017-02-09 15:21:13 +1300

    TL-12200 / MDL-55586 blocks_lp: Translation strings in competencies
    Change-Id: I90a17f4c875cefb0fdd5362b55627648f472d594

--- blocks/lp/lang/en/block_lp.php
+++ blocks/lp/lang/en/block_lp.php
@@ -25,9 +25,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $string['competenciestoreview'] = 'Competencies to review';
-$string['lp:addinstance'] = 'Adding a new block';
-$string['lp:myaddinstance'] = 'Adding the block on the dashboard';
-$string['lp:view'] = 'Viewing the block';
+$string['lp:addinstance'] = 'Add a new learning plans block';
+$string['lp:myaddinstance'] = 'Add a new learning plans block to Dashboard';
+$string['lp:view'] = 'View learning plans block';
+$string['myplans'] = 'My plans';
+$string['noactiveplans'] = 'No active plans at the moment.';
 $string['planstoreview'] = 'Plans to review';
 $string['pluginname'] = 'Learning plans';
 $string['viewmore'] = 'View more...';
