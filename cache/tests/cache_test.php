0570f9940d400f18b6a9dab94b6346d4bdb42cc8
Rajesh Taneja <rajesh@moodle.com>
2017-02-10 09:59:18 +1300

    TL-12523 / MDL-57147 phpunit: Check if time is greater than or equal to
    Change-Id: Ia0d502df0a5ca6dcff93e5e40a3da18657990fdc

--- cache/tests/cache_test.php
+++ cache/tests/cache_test.php
@@ -1788,6 +1788,7 @@ class core_cache_testcase extends advanced_testcase {
     }
 
     /**
+<<<<<<< HEAD
      * Totara test!
      *
      * This test covers the situation where the site is sharing the config and dbmeta cache with sites of the same allversionshash.
@@ -1809,6 +1810,12 @@ class core_cache_testcase extends advanced_testcase {
         $this->resetAfterTest();
     }
 
+    /**
+     * Test static acceleration
+     *
+     * Note: All the assertGreaterThanOrEqual() in this test should be assertGreaterThan() be because of some microtime()
+     * resolution problems under some OSs / PHP versions, we are accepting equal as valid outcome. For more info see MDL-57147.
+     */
     public function test_static_acceleration() {
         $instance = cache_config_testing::instance();
         $instance->phpunit_add_definition('phpunit/accelerated', array(
@@ -1915,7 +1922,7 @@ class core_cache_testcase extends advanced_testcase {
         $this->assertTrue($cache->set('b', $cacheableobject2));
         $staticaccelerationreturntime = $cache->phpunit_static_acceleration_get('a')->propertytime;
         $staticaccelerationreturntimeb = $cache->phpunit_static_acceleration_get('b')->propertytime;
-        $this->assertGreaterThan($startmicrotime, $staticaccelerationreturntime, 'Restore time of static must be newer.');
+        $this->assertGreaterThanOrEqual($startmicrotime, $staticaccelerationreturntime, 'Restore time of static must be newer.');
 
         // Reset the static cache without resetting backing store.
         $cache->phpunit_static_acceleration_purge();
@@ -1923,12 +1930,12 @@ class core_cache_testcase extends advanced_testcase {
         // Get the value from the backend store, populating the static cache.
         $cachevalue = $cache->get('a');
         $this->assertInstanceOf('cache_phpunit_dummy_object', $cachevalue);
-        $this->assertGreaterThan($staticaccelerationreturntime, $cachevalue->propertytime);
+        $this->assertGreaterThanOrEqual($staticaccelerationreturntime, $cachevalue->propertytime);
         $backingstorereturntime = $cachevalue->propertytime;
 
         $results = $cache->get_many(array('b'));
         $this->assertInstanceOf('cache_phpunit_dummy_object', $results['b']);
-        $this->assertGreaterThan($staticaccelerationreturntimeb, $results['b']->propertytime);
+        $this->assertGreaterThanOrEqual($staticaccelerationreturntimeb, $results['b']->propertytime);
         $backingstorereturntimeb = $results['b']->propertytime;
 
         // Obtain the value again and confirm that static cache is using wake_from_cache.
@@ -1936,11 +1943,11 @@ class core_cache_testcase extends advanced_testcase {
         // value is stored serialized in the static acceleration cache.
         $cachevalue = $cache->phpunit_static_acceleration_get('a');
         $this->assertInstanceOf('cache_phpunit_dummy_object', $cachevalue);
-        $this->assertGreaterThan($backingstorereturntime, $cachevalue->propertytime);
+        $this->assertGreaterThanOrEqual($backingstorereturntime, $cachevalue->propertytime);
 
         $results = $cache->get_many(array('b'));
         $this->assertInstanceOf('cache_phpunit_dummy_object', $results['b']);
-        $this->assertGreaterThan($backingstorereturntimeb, $results['b']->propertytime);
+        $this->assertGreaterThanOrEqual($backingstorereturntimeb, $results['b']->propertytime);
 
         /** @var cache_phpunit_application $cache */
         $cache = cache::make('phpunit', 'accelerated2');
