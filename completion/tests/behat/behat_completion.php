412a027047252627735408a61d4c334fe91415ad
Brian Barnes <brian.barnes@totaralms.com>
2017-03-14 10:50:27 +1300

    TL-12837 tests: added namespacing for tablenode
    Change-Id: If1f28bcefccd91e45b38a21f541a3af81cbc70ad

--- completion/tests/behat/behat_completion.php
+++ completion/tests/behat/behat_completion.php
@@ -26,6 +26,7 @@
 // NOTE: no MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
 
 require_once(__DIR__ . '/../../../lib/behat/behat_base.php');
+use Behat\Gherkin\Node\TableNode as TableNode;
 
 /**
  * Steps definitions to deal with course and activities completion.
