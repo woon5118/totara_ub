7ff5e24d0e7d8031653a67bb77d15b170a54cf14
Brendan Cox <brendan.cox@totaralearning.com>
2017-03-15 10:01:22 +1300

    TL-11448 mod_feedback: Fix undefined variable in col_deleteentry
    Change-Id: I6f440b9bdd2abd9af56920d34c2123fb700a7222

--- mod/feedback/classes/responses_table.php
+++ mod/feedback/classes/responses_table.php
@@ -207,7 +207,7 @@ class mod_feedback_responses_table extends table_sql {
         $deleteentryurl = new moodle_url($this->baseurl, ['delete' => $row->id, 'sesskey' => sesskey()]);
         $deleteaction = new confirm_action(get_string('confirmdeleteentry', 'feedback'));
         return $OUTPUT->action_icon($deleteentryurl,
-            new \core\output\flex_icon('delete', array ('alt' => $strdelete)), $deleteaction);
+            new \core\output\flex_icon('delete', array ('alt' => get_string('delete_entry', 'feedback'))), $deleteaction);
     }
 
     /**
