f71632225ccb3959d12675381520ad31791e359e
Brian Barnes <brian.barnes@totaralms.com>
2017-03-16 13:18:56 +1300

    TL-12868 tests: fixed 'I set the following settings for grade item <course>' behat step
    Change-Id: I5f08ec5456f0f4aae935ccdd11fde7b173b83b04

--- grade/tests/behat/behat_grade.php
+++ grade/tests/behat/behat_grade.php
@@ -84,7 +84,7 @@ class behat_grade extends behat_base {
 
         $savechanges = get_string('savechanges', 'grades');
         $edit = behat_context_helper::escape(get_string('edit') . '  ');
-        $linkxpath = "//a[./img[starts-with(@title,$edit) and contains(@title,$gradeitem)]]";
+        $linkxpath = "//a[starts-with(.,$edit) and contains(.,$gradeitem)]";
 
         $this->execute("behat_general::i_click_on", array($this->escape($linkxpath), "xpath_element"));
         $this->execute("behat_forms::i_set_the_following_fields_to_these_values", $data);
