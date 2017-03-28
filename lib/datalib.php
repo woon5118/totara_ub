c29623548cc6a3fa00164214203d702d89a2f119
David Curry <david.curry@totaralms.com>
2017-03-28 14:34:00 +1300

    TL-12964 coursecatlib.php: Update the old catalog search to allow single character searches
    Change-Id: I54ea146b51de12de2d82d5f7ee2d2563d2799842
    Reviewed-on: https://review.totaralms.com/15177
    Reviewed-by: Sam Hemelryk <sam.hemelryk@totaralearning.com>
    Reviewed-by: Maurice Moore <maurice.moore@totaralearning.com>
    Tested-by: Jenkins Automation <jenkins@totaralms.com>

--- lib/datalib.php
+++ lib/datalib.php
@@ -818,17 +818,17 @@ function get_courses_search($searchterms, $sort, $page, $recordsperpage, &$total
             $searchterm = trim($searchterm, '+-');
             $searchterm = preg_quote($searchterm, '|');
             $searchcond[] = "$concat $REGEXP :ss$i";
-            $params['ss'.$i] = "(^|[^a-zA-Z0-9])$searchterm([^a-zA-Z0-9]|$)";
+            $params['ss'.$i] = "(^|[^a-zA-Z0-9])".$DB->sql_like_escape($searchterm)."([^a-zA-Z0-9]|$)";
 
         } else if (substr($searchterm,0,1) == "-") {
             $searchterm = trim($searchterm, '+-');
             $searchterm = preg_quote($searchterm, '|');
             $searchcond[] = "$concat $NOTREGEXP :ss$i";
-            $params['ss'.$i] = "(^|[^a-zA-Z0-9])$searchterm([^a-zA-Z0-9]|$)";
+            $params['ss'.$i] = "(^|[^a-zA-Z0-9])".$DB->sql_like_escape($searchterm)."([^a-zA-Z0-9]|$)";
 
         } else {
             $searchcond[] = $DB->sql_like($concat,":ss$i", false, true, $NOT);
-            $params['ss'.$i] = "%$searchterm%";
+            $params['ss'.$i] = "%".$DB->sql_like_escape($searchterm)."%";
         }
     }
 
