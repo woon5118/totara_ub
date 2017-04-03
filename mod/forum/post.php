ba2f58a312ba56e52f6bf8ecc814136c6f9d7b23
Marina Glancy <marina@moodle.com>
2017-04-03 09:58:48 +0800

    TL-22807 / MDL-46929 mod_forum: small fixes for post tagging

--- mod/forum/post.php
+++ mod/forum/post.php
@@ -775,8 +775,6 @@ if ($mform_post->is_cancelled()) {
             $discussionurl = new moodle_url("/mod/forum/discuss.php", array('d' => $discussion->id), 'p' . $fromform->id);
         }
 
-        core_tag_tag::set_item_tags('mod_forum', 'forum_posts', $fromform->id, $modcontext, $fromform->tags);
-
         $params = array(
             'context' => $modcontext,
             'objectid' => $fromform->id,
@@ -828,10 +826,6 @@ if ($mform_post->is_cancelled()) {
                 $discussionurl = new moodle_url("/mod/forum/view.php", array('f' => $forum->id), 'p'.$fromform->id);
             } else {
                 $discussionurl = new moodle_url("/mod/forum/discuss.php", array('d' => $discussion->id), 'p'.$fromform->id);
-            }
-
-            if (core_tag_tag::is_enabled('mod_forum', 'forum_posts') && isset($data->tags)) {
-                core_tag_tag::set_item_tags('mod_forum', 'forum_posts', $fromform->id, $modcontext, $fromform->tags);
             }
 
             $params = array(
@@ -969,8 +963,6 @@ if ($mform_post->is_cancelled()) {
                 ($forum->completiondiscussions || $forum->completionposts)) {
             $completion->update_state($cm, COMPLETION_COMPLETE);
         }
-
-        core_tag_tag::set_item_tags('mod_forum', 'forum_posts', $fromform->firstpost, $modcontext, $fromform->tags);
 
         // Redirect back to the discussion.
         redirect(
