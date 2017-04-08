685a749cd40c4be5c0b28191ddc8debb1c46c86a
Petr Skoda <petr.skoda@totaralearning.com>
2017-04-08 20:44:44 +1200

    TL-12978 admin: remove accidentally reintroduced code for addon updates
    Change-Id: I5c91787bcc2d18c274b867510454a2fd5ffd3a8d

--- admin/settings/server.php
+++ admin/settings/server.php
@@ -291,22 +291,4 @@ $temp->add(new admin_setting_configselect('emailfromvia', new lang_string('email
 
 $ADMIN->add('email', $temp);
 
-// "update notifications" settingpage
-if (empty($CFG->disableupdatenotifications)) {
-    $temp = new admin_settingpage('updatenotifications', new lang_string('updatenotifications', 'core_admin'));
-    $temp->add(new admin_setting_configcheckbox('updateautocheck', new lang_string('updateautocheck', 'core_admin'),
-                                                new lang_string('updateautocheck_desc', 'core_admin'), 1));
-    $temp->add(new admin_setting_configselect('updateminmaturity', new lang_string('updateminmaturity', 'core_admin'),
-                                              new lang_string('updateminmaturity_desc', 'core_admin'), MATURITY_STABLE,
-                                              array(
-                                                  MATURITY_ALPHA  => new lang_string('maturity'.MATURITY_ALPHA, 'core_admin'),
-                                                  MATURITY_BETA   => new lang_string('maturity'.MATURITY_BETA, 'core_admin'),
-                                                  MATURITY_RC     => new lang_string('maturity'.MATURITY_RC, 'core_admin'),
-                                                  MATURITY_STABLE => new lang_string('maturity'.MATURITY_STABLE, 'core_admin'),
-                                              )));
-    $temp->add(new admin_setting_configcheckbox('updatenotifybuilds', new lang_string('updatenotifybuilds', 'core_admin'),
-                                                new lang_string('updatenotifybuilds_desc', 'core_admin'), 0));
-    $ADMIN->add('server', $temp);
-}
-
 } // end of speedup
