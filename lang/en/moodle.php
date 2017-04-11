18688ef44c50d7188edfe159ae066a68c99c7ab9
sam marshall <s.marshall@open.ac.uk>
2017-04-11 11:01:42 +0100

    TL-22536 / MDL-58567 Upgrade: Show upgrade times

--- lang/en/moodle.php
+++ lang/en/moodle.php
@@ -1883,6 +1883,7 @@ $string['subcategory'] = 'Subcategory';
 $string['subcategoryof'] = 'Subcategory of {$a}';
 $string['submit'] = 'Submit';
 $string['success'] = 'Success';
+$string['successduration'] = 'Success ({$a} seconds)';
 $string['summary'] = 'Summary';
 $string['summary_help'] = 'The idea of a summary is a short text to prepare learners for the activities within the topic or week. The text is shown on the course page under the section name.';
 $string['summaryof'] = 'Summary of {$a}';
