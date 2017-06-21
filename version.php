83ed59ae3319998c5f276f7d935af3c2e57f7d34
Sam Hemelryk <sam.hemelryk@totaralearning.com>
2017-06-21 18:14:17 +1200

    TL-885 version.php: Bumping development version

--- version.php
+++ version.php
@@ -36,7 +36,7 @@ $version  = 2016120503.00;              // YYYYMMDD      = weekly release date o
 $release  = '3.2.3 (Build: 20170508)'; // Human-friendly version name
 
 $branch   = '32';                       // This version's branch.
-$maturity = MATURITY_EVERGREEN;         // This version's maturity level.
+$maturity = MATURITY_ALPHA;             // This version's maturity level.
 
 
 // TOTARA VERSION INFORMATION
@@ -47,7 +47,7 @@ $maturity = MATURITY_EVERGREEN;         // This version's maturity level.
 $TOTARA = new stdClass();
 
 $TOTARA->version    = '10dev';         // Please keep as string.
-$TOTARA->build      = '20170621.00';  // Please keep as string.
+$TOTARA->build      = '20170622.00';   // Please keep as string.
 
 if ($maturity == MATURITY_EVERGREEN) {
     $TOTARA->release = "Evergreen (Build: {$TOTARA->build})";
