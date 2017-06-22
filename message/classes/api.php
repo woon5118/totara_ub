80baf64be28c87ec2eee26ed1173c67e480acb45
Neill Magill <neill.magill@nottingham.ac.uk>
2017-06-22 10:16:32 +0100

    TL-16020 / MDL-59317 message: Message page taking long time to load
    Change-Id: Ia0dc81b8131014536689f149c890fd9a35161e5e
    Reviewed-on: https://review.totaralms.com/17193
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Sam Hemelryk <sam.hemelryk@totaralearning.com>

--- message/classes/api.php
+++ message/classes/api.php
@@ -284,7 +284,14 @@ class api {
                         FROM {message}
                         WHERE
                             (useridto = ? AND timeusertodeleted = 0 AND notification = 0)
-                            OR
+                        UNION ALL
+                        SELECT
+                            id, useridfrom, useridto, subject, fullmessage, fullmessageformat,
+                            fullmessagehtml, smallmessage, notification, contexturl,
+                            contexturlname, timecreated, timeuserfromdeleted, timeusertodeleted,
+                            component, eventtype, 0 as timeread
+                        FROM {message}
+                        WHERE
                             (useridfrom = ? AND timeuserfromdeleted = 0 AND notification = 0)
                         UNION ALL
                         SELECT
@@ -295,7 +302,14 @@ class api {
                         FROM {message_read}
                         WHERE
                             (useridto = ? AND timeusertodeleted = 0 AND notification = 0)
-                            OR
+                        UNION ALL
+                        SELECT
+                            id, useridfrom, useridto, subject, fullmessage, fullmessageformat,
+                            fullmessagehtml, smallmessage, notification, contexturl,
+                            contexturlname, timecreated, timeuserfromdeleted, timeusertodeleted,
+                            component, eventtype, timeread
+                        FROM {message_read}
+                        WHERE
                             (useridfrom = ? AND timeuserfromdeleted = 0 AND notification = 0)";
         $allmessagesparams = [$userid, $userid, $userid, $userid];
 
@@ -351,7 +365,11 @@ class api {
                         FROM {message}
                         WHERE
                             (useridto = ? AND timeusertodeleted = 0 AND notification = 0)
-                            OR
+                            AND timecreated $timecreatedsql
+                        UNION ALL
+                        SELECT id, useridfrom, useridto, timecreated
+                        FROM {message}
+                        WHERE
                             (useridfrom = ? AND timeuserfromdeleted = 0 AND notification = 0)
                             AND timecreated $timecreatedsql
                         UNION ALL
@@ -359,14 +377,19 @@ class api {
                         FROM {message_read}
                         WHERE
                             (useridto = ? AND timeusertodeleted = 0 AND notification = 0)
-                            OR
+                            AND timecreated $timecreatedsql
+                        UNION ALL
+                        SELECT id, useridfrom, useridto, timecreated
+                        FROM {message_read}
+                        WHERE
                             (useridfrom = ? AND timeuserfromdeleted = 0 AND notification = 0)
                             AND timecreated $timecreatedsql";
         $messageidsql = "SELECT $convosig, max(id) as id, timecreated
                          FROM ($allmessagestimecreated) x
                          WHERE $messageidwhere
                          GROUP BY $convocase, timecreated";
-        $messageidparams = array_merge([$userid, $userid], $timecreatedparams, [$userid, $userid], $timecreatedparams);
+        $messageidparams = array_merge([$userid], $timecreatedparams, [$userid], $timecreatedparams,
+                [$userid], $timecreatedparams, [$userid], $timecreatedparams);
         $messageidrecords = $DB->get_records_sql($messageidsql, $messageidparams);
 
         // Ok, let's recap. We've pulled a descending ordered list of conversations by latest time created
