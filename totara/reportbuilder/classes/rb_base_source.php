81bff5c4c443774ec0e993995d41afbf7ca9c82a
Oleg Demeshev <oleg.demeshev@totaralms.com>
2017-06-22 16:21:34 +1200

    TL-14744 reportbuilder: Fix enhanced course catalog when all filters disabled
    Change-Id: I3c5371bcf3f3c2bb9c392f8fea3f43a373cd89ae
    Reviewed-on: https://review.totaralms.com/15821
    Reviewed-by: Sam Hemelryk <sam.hemelryk@totaralearning.com>
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Maria Torres <maria.torres@totaralearning.com>

--- totara/reportbuilder/classes/rb_base_source.php
+++ totara/reportbuilder/classes/rb_base_source.php
@@ -1218,6 +1218,7 @@ abstract class rb_base_source {
         global $CFG;
 
         require_once($CFG->dirroot . '/lib/pear/HTML/QuickForm/button.php');
+        require_once($CFG->dirroot . '/lib/pear/HTML/QuickForm/static.php');
 
         $retval = array();
         foreach ($inlineenrolments as $inlineenrolment) {
