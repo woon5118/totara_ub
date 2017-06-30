63d709720fdde8aac76ab6c88002536c87321434
sam marshall <s.marshall@open.ac.uk>
2017-06-30 11:15:26 +0100

    TL-22543 / MDL-59427 MUC: cache_helper::purge_all should reset adhoc caches

--- cache/classes/helper.php
+++ cache/classes/helper.php
@@ -498,6 +498,9 @@ class cache_helper {
         foreach ($config->get_all_stores() as $store) {
             self::purge_store($store['name'], $config);
         }
+        foreach ($factory->get_adhoc_caches_in_use() as $cache) {
+            $cache->purge();
+        }
         // Totara: the factory needs to know that the stores were purged to invalidate all caches!
         $factory->purged_all_stores();
     }
