678956f65c663791f15e269a18c4e541eaf4ea4c
Sam Hemelryk <sam.hemelryk@totaralearning.com>
2017-07-19 20:43:19 +1200

    TL-885 version.php: Bump up dev build after stable release

--- version.php
+++ version.php
@@ -47,7 +47,7 @@ $maturity = MATURITY_EVERGREEN;         // This version's maturity level.
 $TOTARA = new stdClass();
 
 $TOTARA->version    = '10dev';         // Please keep as string.
-$TOTARA->build      = '20170719.00';   // Please keep as string.
+$TOTARA->build      = '20170719.01';   // Please keep as string.
 
 if ($maturity == MATURITY_EVERGREEN) {
     $TOTARA->release = "Evergreen (Build: {$TOTARA->build})";
