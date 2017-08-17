b0153c1ebac1a12e637d0ef2e1ed81f44f04b040
Brian Barnes <brian.barnes@totaralms.com>
2017-08-17 15:45:29 +1200

    TL-15841 mod_wiki: re-applied TL-14365
    Change-Id: I3d84035b21aa6afd7c89b99dc7be27ffe3258837
    Reviewed-on: https://review.totaralms.com/16560
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Sam Hemelryk <sam.hemelryk@totaralearning.com>
    Reviewed-by: Brendan Cox <brendan.cox@totaralearning.com>

--- mod/wiki/create.php
+++ mod/wiki/create.php
@@ -27,11 +27,22 @@ require_once($CFG->dirroot . '/mod/wiki/pagelib.php');
 // 'create' action will create a new page in db, and redirect to
 // page editing page.
 $action = optional_param('action', 'new', PARAM_TEXT);
+$title = optional_param('title', null, PARAM_TEXT);
 $wid = optional_param('wid', 0, PARAM_INT);
 $swid = optional_param('swid', 0, PARAM_INT);
 $group = optional_param('group', 0, PARAM_INT);
 $uid = optional_param('uid', 0, PARAM_INT);
-
+$url = new moodle_url('/mod/wiki/create.php');
+if ($wid !== 0) {
+    $url->param('wid', $wid);
+}
+if ($uid !== 0) {
+    $url->param('uid', $uid);
+}
+if ($title !== null) {
+    $url->param('title', $title);
+}
+$PAGE->set_url($url);
 
 // 'create' action must be submitted by moodle form
 // so sesskey must be checked
@@ -86,8 +97,9 @@ $course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST)
 require_login($course, true, $cm);
 
 // The title of the new page, cannot be empty.
-$title = optional_param('title', get_string('newpage', 'wiki'), PARAM_TEXT);
-$PAGE->set_url('/mod/wiki/create.php', array('wid' => $wid, 'uid' => $uid, 'title' => $title));
+if ($title === null) {
+    $title = get_string('newpage', 'wiki');
+}
 $wikipage = new page_wiki_create($wiki, $subwiki, $cm);
 
 if (!empty($swid)) {
