7da50aa6cbf0db7c9ba460a8da5c69db0400800c
David Curry <david.curry@totaralearning.com>
2017-09-13 13:02:44 +1200

    TL-16126 mod_choice: Fix archive function used in certification window opening
    Change-Id: If48d4092dd50ea613a06615da9ee9adfa8cc088c
    Reviewed-on: https://review.totaralms.com/16852
    Reviewed-by: Nathan Lewis <nathan.lewis@totaralearning.com>
    Reviewed-by: Brendan Cox <brendan.cox@totaralearning.com>
    Tested-by: Jenkins Automation <jenkins@totaralms.com>

--- mod/choice/lib.php
+++ mod/choice/lib.php
@@ -104,37 +104,38 @@ function choice_user_complete($course, $user, $mod, $choice) {
 }
 
 /**
- * Archives user's assignments for a course
+ * Reset a users completion status in a choice so they can retake the activity
+ * mainly used for certification recerts
  *
  * @param int $userid
  * @param int $courseid
+ * @param int $windowopens  - Unused, the timestamp for when a recertification window opens
  */
 function choice_archive_completion($userid, $courseid, $windowopens = NULL) {
     global $DB;
 
     $sql = "SELECT ca.id AS answerid,
                     c.id AS choiceid
-            FROM {choice_answers} ca
-            JOIN {choice} c ON c.id = ca.choiceid AND c.course = :courseid
-            WHERE ca.userid = :userid";
+              FROM {choice_answers} ca
+              JOIN {choice} c ON c.id = ca.choiceid
+               AND c.course = :courseid
+             WHERE ca.userid = :userid";
     $params = array('userid' => $userid, 'courseid' => $courseid);
 
     if ($submissions = $DB->get_records_sql($sql, $params)) {
         $course = $DB->get_record('course', array('id' => $courseid), '*', MUST_EXIST);
-        // Answers to be deleted.
-        $deleteanswers = array();
         // Create the course completion info.
         $completion = new completion_info($course);
 
         foreach ($submissions as $submission) {
             $cm = get_coursemodule_from_instance('choice', $submission->choiceid, $course->id);
-            $deleteanswers[] = $submission->answerid;
+            // Delete the answers immediately so they aren't considered when we recheck completion in update state.
+            $DB->delete_records('choice_answers', array('id' => $submission->answerid));
             // Reset viewed.
             $completion->set_module_viewed_reset($cm, $userid);
             // And reset completion, in case viewed is not a required condition.
             $completion->update_state($cm, COMPLETION_INCOMPLETE, $userid);
         }
-        $DB->delete_records_list('choice_answers', 'id', $deleteanswers);
         $completion->invalidatecache($courseid, $userid, true);
     }
 
