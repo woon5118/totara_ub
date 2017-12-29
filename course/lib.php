84d0b712c361c869bb0412c90e5ff43e158b7591
Farhan Karmali <farhankarmali@Farhans-MacBook-Air.local>
2017-12-29 20:13:48 +0530

    TL-18113 / MDL-37390 course: Set startdate properly for approved course

--- course/lib.php
+++ course/lib.php
@@ -2914,6 +2914,7 @@ class course_request {
         $data->visibleold         = $data->visible;
         $data->lang               = $courseconfig->lang;
         $data->enablecompletion   = $courseconfig->enablecompletion;
+        $data->startdate          = usergetmidnight(time());
 
         $course = create_course($data);
         $context = context_course::instance($course->id, MUST_EXIST);
