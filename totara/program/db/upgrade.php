08da768601a6b9debef7c1f8d37649c848f5bc65
Andrew McGhie <andrew.mcghie@totaralearning.com>
2018-01-05 16:44:23 +1300

    TL-16662 totara_program: deleted course sets that have no course set courses

--- totara/program/db/upgrade.php
+++ totara/program/db/upgrade.php
@@ -180,5 +180,23 @@ function xmldb_totara_program_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2017112000, 'totara', 'totara_program');
     }
 
+    // Does part of the fix from TL-6372 again as on certain execution paths it could be missed.
+    if ($oldversion < 2018010800) {
+        // Get IDs of empty coursesets so we can delete them.
+        $emptycoursesets = $DB->get_fieldset_sql('SELECT cs.id 
+                                                      FROM {prog_courseset} cs 
+                                                      LEFT JOIN {prog_courseset_course} csc 
+                                                        ON cs.id = csc.coursesetid 
+                                                      WHERE csc.coursesetid IS NULL GROUP BY cs.id');
+
+        if (!empty($emptycoursesets)) {
+            list($insql, $inparams) = $DB->get_in_or_equal($emptycoursesets);
+            $DB->delete_records_select('prog_courseset', "id {$insql}", $inparams);
+        }
+
+        // Main savepoint reached.
+        totara_upgrade_mod_savepoint(true, 2018010800, 'totara_program');
+    }
+
     return true;
 }
