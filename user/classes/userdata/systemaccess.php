8f50d9426ef033bfce6d81e30af9fc0a1eb1b2b1
Fabian Derschatta <fabian.derschatta@totaralearning.com>
2018-02-09 15:25:14 +1300

    TL-16328 user: Implemented userdata item for user's system access

--- user/classes/userdata/systemaccess.php
+++ user/classes/userdata/systemaccess.php
@@ -23,6 +23,7 @@
 
 namespace core_user\userdata;
 
+use totara_userdata\userdata\export;
 use totara_userdata\userdata\target_user;
 
 defined('MOODLE_INTERNAL') || die();
@@ -31,13 +32,14 @@ defined('MOODLE_INTERNAL') || die();
  * Various use access timestamps.
  */
 class systemaccess extends \totara_userdata\userdata\item {
+
     /**
      * String used for human readable name of this item.
      *
      * @return array parameters of get_string($identifier, $component) to get full item name and optionally help.
      */
     public static function get_fullname_string() {
-        return ['userdata_core_user_systemaccess', 'totara_userdata'];
+        return ['userdataitem-user-systemaccess', 'core'];
     }
 
     /**
@@ -71,27 +73,31 @@ class systemaccess extends \totara_userdata\userdata\item {
     protected static function purge(target_user $user, \context $context) {
         global $DB;
 
-        $updates = array();
-        if ($user->firstaccess != 0) {
+        $updates = [];
+        if (!empty($user->firstaccess)) {
             $updates['firstaccess'] = 0;
         }
-        if ($user->lastaccess != 0) {
+        if (!empty($user->lastaccess)) {
             $updates['lastaccess'] = 0;
         }
-        if ($user->lastlogin != 0) {
+        if (!empty($user->lastlogin)) {
             $updates['lastlogin'] = 0;
         }
-        if ($user->currentlogin != 0) {
+        if (!empty($user->currentlogin)) {
             $updates['currentlogin'] = 0;
         }
-        if ($updates) {
-            $update['id'] = $user->id;
-            $DB->update_record('user', (object)$update);
+        if (!empty($user->lastip) && $user->lastip != '0.0.0.0') {
+            $updates['lastip'] = '';
         }
 
-        $DB->delete_records('user_lastaccess', array('userid' => $user->id));
+        if (!empty($updates)) {
+            $updates['id'] = $user->id;
+            $DB->update_record('user', (object)$updates);
+        }
 
-        // NOTE: do not trigger events, user must be laready deleted.
+        $DB->delete_records('user_lastaccess', ['userid' => $user->id]);
+
+        // NOTE: do not trigger events, user must be already deleted.
 
         return self::RESULT_STATUS_SUCCESS;
     }
@@ -110,26 +116,28 @@ class systemaccess extends \totara_userdata\userdata\item {
      *
      * @param target_user $user
      * @param \context $context restriction for exporting i.e., system context for everything and course context for course export
-     * @return \totara_userdata\userdata\export|int result object or integer error code self::RESULT_STATUS_ERROR or self::RESULT_STATUS_SKIPPED
+     * @return export|int result object or integer error code self::RESULT_STATUS_ERROR or self::RESULT_STATUS_SKIPPED
      */
     protected static function export(target_user $user, \context $context) {
         global $DB;
 
-        $export = new \totara_userdata\userdata\export();
+        $export = new export();
 
-        if ($user->firstaccess) {
-            $export->data['firstaccess'] = $user->firstaccess;
-        }
-        if ($user->lastaccess) {
-            $export->data['lastaccess'] = $user->lastaccess;
-        }
-        if ($user->lastlogin) {
-            $export->data['lastlogin'] = $user->lastlogin;
-        }
-        if ($user->currentlogin) {
-            $export->data['currentlogin'] = $user->currentlogin;
-        }
-        $export->data['courses'] = $DB->get_records_menu('user_lastaccess', array('userid' => $user->id), 'courseid ASC', 'courseid, timeaccess');
+        $courses = $DB->get_records_menu(
+            'user_lastaccess',
+            ['userid' => $user->id],
+            'courseid ASC',
+            'courseid, timeaccess'
+        );
+
+        $export->data = [
+            'firstaccess' => !empty($user->firstaccess) ? $user->firstaccess : '',
+            'lastaccess' => !empty($user->lastaccess) ? $user->lastaccess : '',
+            'lastlogin' => !empty($user->lastlogin) ? $user->lastlogin : '',
+            'currentlogin' => !empty($user->currentlogin) ? $user->currentlogin : '',
+            'lastip' => (!empty($user->lastip) && $user->lastip != '0.0.0.0') ? $user->lastip : '',
+            'courses' => $courses
+        ];
 
         return $export;
     }
@@ -149,27 +157,20 @@ class systemaccess extends \totara_userdata\userdata\item {
      *
      * @param target_user $user
      * @param \context $context restriction for counting i.e., system context for everything and course context for course data
-     * @return int  integer is the count >= 0, negative number is error result self::RESULT_STATUS_ERROR or self::RESULT_STATUS_SKIPPED
+     * @return int is the count >= 0, negative number is error result self::RESULT_STATUS_ERROR or self::RESULT_STATUS_SKIPPED
      */
     protected static function count(target_user $user, \context $context) {
         global $DB;
 
-        $count = 0;
+        $count = [
+            intval(!empty($user->firstaccess)),
+            intval(!empty($user->lastaccess)),
+            intval(!empty($user->lastlogin)),
+            intval(!empty($user->currentlogin)),
+            intval(!empty($user->lastip) && $user->lastip != '0.0.0.0'),
+            $DB->count_records('user_lastaccess', ['userid' => $user->id])
+        ];
 
-        if ($user->firstaccess) {
-            $count++;
-        }
-        if ($user->lastaccess) {
-            $count++;
-        }
-        if ($user->lastlogin) {
-            $count++;
-        }
-        if ($user->currentlogin) {
-            $count++;
-        }
-
-        $count = $count + $DB->count_records('user_lastaccess', array('userid' => $user->id));
-        return $count;
+        return array_sum($count);
     }
 }
