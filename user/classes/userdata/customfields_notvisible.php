bc1cb1a8e9e7bf5fd5af0aadaab7490934ca8570
Fabian Derschatta <fabian.derschatta@totaralearning.com>
2018-02-12 18:02:27 +1300

    TL-16328 user: Split purging and exporting of users custom fields based on visibility

--- user/classes/userdata/customfields_notvisible.php
+++ user/classes/userdata/customfields_notvisible.php
@@ -18,6 +18,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @author Petr Skoda <petr.skoda@totaralearning.com>
+ * @author Fabian Derschatta <fabian.derschatta@totaralearning.com>
  * @package core_user
  */
 
@@ -29,9 +30,11 @@ use totara_userdata\userdata\target_user;
 defined('MOODLE_INTERNAL') || die();
 
 /**
- * All custom profile fields.
+ * All custom profile fields which are not by default visible to the user.
+ * This item only includes 'not visible' fields which need "moodle/user:viewalldetails" capability in user context
+ * or "moodle/course:viewhiddenuserfields" capability in course context to view them.
  */
-class customfields extends \totara_userdata\userdata\item {
+class customfields_notvisible extends \totara_userdata\userdata\item {
 
     /**
      * String used for human readable name of this item.
@@ -39,7 +42,7 @@ class customfields extends \totara_userdata\userdata\item {
      * @return array parameters of get_string($identifier, $component) to get full item name and optionally help.
      */
     public static function get_fullname_string() {
-        return ['userdataitem-user-customfields', 'core'];
+        return ['userdataitem-user-customfields_notvisible', 'core'];
     }
 
     /**
@@ -71,10 +74,22 @@ class customfields extends \totara_userdata\userdata\item {
      * @return int result self::RESULT_STATUS_SUCCESS, self::RESULT_STATUS_ERROR or status::RESULT_STATUS_SKIPPED
      */
     protected static function purge(target_user $user, \context $context) {
-        global $DB;
+        global $DB, $CFG;
 
-        if ($DB->record_exists('user_info_data', ['userid' => $user->id])) {
-            $DB->delete_records('user_info_data', ['userid' => $user->id]);
+        require_once($CFG->dirroot . '/user/profile/lib.php');
+
+        $sql = "SELECT d.id
+                  FROM {user_info_data} d
+                  JOIN {user_info_field} f ON f.id = d.fieldid AND f.visible = :visiblenone
+                 WHERE d.userid = :userid";
+        $recordids = $DB->get_fieldset_sql($sql, ['visiblenone' => PROFILE_VISIBLE_NONE, 'userid' => $user->id]);
+
+        if (!empty($recordids)) {
+            list($idselect, $params) = $DB->get_in_or_equal($recordids, SQL_PARAMS_NAMED);
+            $params['userid'] = $user->id;
+            $sql = "DELETE FROM {user_info_data} WHERE id $idselect AND userid = :userid";
+            $DB->execute($sql, $params);
+
             if (!$user->deleted) {
                 \core\event\user_updated::create_from_userid($user->id)->trigger();
             }
@@ -100,15 +115,18 @@ class customfields extends \totara_userdata\userdata\item {
      * @return export|int result object or integer error code self::RESULT_STATUS_ERROR or self::RESULT_STATUS_SKIPPED
      */
     protected static function export(target_user $user, \context $context) {
-        global $DB;
+        global $DB, $CFG;
+
+        require_once($CFG->dirroot . '/user/profile/lib.php');
 
         $export = new export();
 
         $sql = "SELECT f.shortname, d.data
                   FROM {user_info_data} d
-                  JOIN {user_info_field} f ON f.id = d.fieldid
+                  JOIN {user_info_field} f ON f.id = d.fieldid AND f.visible = :visiblenone
                  WHERE d.userid = :userid";
-        $fields = $DB->get_recordset_sql($sql, ['userid' => $user->id]);
+        $fields = $DB->get_recordset_sql($sql, ['visiblenone' => PROFILE_VISIBLE_NONE, 'userid' => $user->id]);
+
         foreach ($fields as $field) {
             $export->data[] = ['shortname' => $field->shortname, 'data' => $field->data];
         }
@@ -135,8 +153,15 @@ class customfields extends \totara_userdata\userdata\item {
      * @return int is the count >= 0, negative number is error result self::RESULT_STATUS_ERROR or self::RESULT_STATUS_SKIPPED
      */
     protected static function count(target_user $user, \context $context) {
-        global $DB;
+        global $DB, $CFG;
 
-        return (int)$DB->count_records('user_info_data', ['userid' => $user->id]);
+        require_once($CFG->dirroot . '/user/profile/lib.php');
+
+        $sql = "SELECT COUNT(d.id)
+                  FROM {user_info_data} d
+                  JOIN {user_info_field} f ON f.id = d.fieldid AND f.visible = :visiblenone
+                 WHERE d.userid = :userid";
+
+        return $DB->count_records_sql($sql, ['visiblenone' => PROFILE_VISIBLE_NONE, 'userid' => $user->id]);
     }
 }
