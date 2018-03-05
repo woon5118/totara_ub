1b6c433001eb5d2516b8e5bd56e05a1c93e43d3f
Guillermo Alvarez <guillermo.alvarez@blackboard.com>
2018-03-05 14:28:46 -0500

    TL-18014 / MDL-61129 survey: Add colgroup attribute at survey question tables

--- mod/survey/lib.php
+++ mod/survey/lib.php
@@ -605,6 +605,7 @@ function survey_print_multi($question) {
         $P = "";
     }
 
+    echo "<colgroup colspan=\"7\"></colgroup>";
     echo "<tr class=\"smalltext\"><th scope=\"row\">$strresponses</th>";
     echo "<th scope=\"col\" class=\"hresponse\">". get_string('notyetanswered', 'survey'). "</th>";
     foreach ($options as $key => $val) {
@@ -612,7 +613,7 @@ function survey_print_multi($question) {
     }
     echo "</tr>\n";
 
-    echo "<tr><th scope=\"col\" colspan=\"7\">$question->intro</th></tr>\n";
+    echo "<tr><th scope=\"colgroup\" colspan=\"7\">$question->intro</th></tr>\n";
 
     $subquestions = survey_get_subquestions($question);
 
