7d5229f6e13dd9dab6e39881e330f0a55b5415e5
Sam Hemelryk <sam.hemelryk@totaralearning.com>
2018-03-12 07:30:36 +1300

    TL-17171 version.php: Tagging version 11.0

--- version.php
+++ version.php
@@ -36,7 +36,7 @@ $version  = 2016120505.01;              // YYYYMMDD      = weekly release date o
 $release  = '3.2.5 (Build: 20170911)';  // Human-friendly version name
 
 $branch   = '32';                       // This version's branch.
-$maturity = MATURITY_RC;             // This version's maturity level.
+$maturity = MATURITY_STABLE;             // This version's maturity level.
 
 
 // TOTARA VERSION INFORMATION
@@ -46,8 +46,8 @@ $maturity = MATURITY_RC;             // This version's maturity level.
 
 $TOTARA = new stdClass();
 
-$TOTARA->version    = '11dev';         // Please keep as string.
-$TOTARA->build      = '20180305.00';   // Please keep as string.
+$TOTARA->version    = '11.0';          // Please keep as string.
+$TOTARA->build      = '20180312.00';   // Please keep as string.
 
 if ($maturity == MATURITY_EVERGREEN) {
     $TOTARA->release = "Evergreen (Build: {$TOTARA->build})";
