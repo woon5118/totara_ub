4d2c9f5851679e1e0ada0093411a0c96b59170b2
Petr Skoda <petr.skoda@totaralearning.com>
2018-03-20 11:29:13 +1300

    TL-17261 auth_email: reintroduce Totara changes

--- auth/email/settings.php
+++ auth/email/settings.php
@@ -31,13 +31,25 @@ if ($ADMIN->fulltree) {
         new lang_string('auth_emaildescription', 'auth_email')));
 
     $options = array(
-        new lang_string('no'),
-        new lang_string('yes'),
+        0 => new lang_string('no'),
+        1 => new lang_string('yes'),
     );
 
     $settings->add(new admin_setting_configselect('auth_email/recaptcha',
         new lang_string('auth_emailrecaptcha_key', 'auth_email'),
         new lang_string('auth_emailrecaptcha', 'auth_email'), 0, $options));
+
+    // NOTE: the whole email signup and related stuff will be removed in T12,
+    //       we will just keep the email auth for existing users.
+    $settings->add(new admin_setting_configselect('totara_job/allowsignupposition',
+        new lang_string('allowsignupposition', 'totara_job'),
+        new lang_string('allowsignupposition_help', 'totara_job'), 0, $options));
+    $settings->add(new admin_setting_configselect('totara_job/allowsignuporganisation',
+        new lang_string('allowsignuporganisation', 'totara_job'),
+        new lang_string('allowsignuporganisation_help', 'totara_job'), 0, $options));
+    $settings->add(new admin_setting_configselect('totara_job/allowsignupmanager',
+        new lang_string('allowsignupmanager', 'totara_job'),
+        new lang_string('allowsignupmanager_help', 'totara_job'), 0, $options));
 
     // Display locking / mapping of profile fields.
     $authplugin = get_auth_plugin($this->name);
