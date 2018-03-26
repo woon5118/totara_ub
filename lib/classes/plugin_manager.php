2ec399e273c249991aa1e4f99a6520e04308c022
Mark Nelson <markn@moodle.com>
2018-03-26 16:46:37 +0800

    TL-19044 / MDL-60416 core: added 'gradebookservices' to plugin list

--- lib/classes/plugin_manager.php
+++ lib/classes/plugin_manager.php
@@ -1387,7 +1387,7 @@ class core_plugin_manager {
             ),
 
             'ltiservice' => array(
-                'memberships', 'profile', 'toolproxy', 'toolsettings'
+                'gradebookservices', 'memberships', 'profile', 'toolproxy', 'toolsettings'
             ),
 
             'media' => array(
