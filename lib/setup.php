7fc144e6a565cff76f03b5313ef9487c76648c19
Petr Skoda <petr.skoda@totaralearning.com>
2018-03-29 15:01:15 +1300

    TL-17330 upgrade: migrate only from Moodle 3.2.6

--- lib/setup.php
+++ lib/setup.php
@@ -411,8 +411,8 @@ if (!defined('MOODLE_INTERNAL')) { // Necessary because cli installer has to def
 
 // Totara: we support migration from this particular Moodle release only.
 if (!defined('MOODLE_MIGRATION_VERSION')) {
-    define('MOODLE_MIGRATION_VERSION', '2016120506.00'); // Keep as string to simplify comparison with DB data.
-    define('MOODLE_MIGRATION_RELEASE', '3.2.6 (Build: 20171113)');
+    define('MOODLE_MIGRATION_VERSION', '2016120507.00'); // Keep as string to simplify comparison with DB data.
+    define('MOODLE_MIGRATION_RELEASE', '3.2.7 (Build: 20180115)');
 }
 
 // core_component can be used in any scripts, it does not need anything else.
