34f63a41b6a3ac0ec330f5e49f8f104a51cc3f15
Matthias Bonk <matthias.bonk@totaralearning.com>
2018-04-03 14:54:59 +1200

    TL-17398 glossary: Tests and adjustments for glossary entries userdata item

--- mod/glossary/classes/userdata/entries.php
+++ mod/glossary/classes/userdata/entries.php
@@ -18,11 +18,20 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @author Petr Skoda <petr.skoda@totaralearning.com>
+ * @author Matthias Bonk <matthias.bonk@totaralearning.com>
  * @package mod_glossary
  */
 
 namespace mod_glossary\userdata;
 
+use comment;
+use completion_info;
+use context;
+use context_module;
+use mod_glossary\event\entry_deleted;
+use mod_glossary\local\concept_cache;
+use rating_manager;
+use totara_userdata\userdata\export;
 use totara_userdata\userdata\target_user;
 
 defined('MOODLE_INTERNAL') || die();
@@ -75,10 +84,10 @@ class entries extends \totara_userdata\userdata\item {
      *       it is also possible that we do not know the original user context id.
      *
      * @param target_user $user
-     * @param \context $context restriction for purging e.g., system context for everything, course context for purging one course
+     * @param context $context restriction for purging e.g., system context for everything, course context for purging one course
      * @return int result self::RESULT_STATUS_SUCCESS, self::RESULT_STATUS_ERROR or self::RESULT_STATUS_SKIPPED
      */
-    protected static function purge(target_user $user, \context $context) {
+    protected static function purge(target_user $user, context $context) {
         global $DB, $CFG;
         require_once($CFG->dirroot . '/mod/glossary/lib.php');
         require_once($CFG->dirroot . '/mod/glossary/rsslib.php');
@@ -86,8 +95,8 @@ class entries extends \totara_userdata\userdata\item {
         require_once($CFG->dirroot . '/comment/lib.php');
 
         $fs = get_file_storage();
-        $rm = new \rating_manager();
-        $glossaries = array();
+        $rm = new rating_manager();
+        $glossaries = [];
 
         // Remove original exported items first.
         $join = self::get_activities_join($context, 'glossary', 'e.sourceglossaryid', 'g');
@@ -96,24 +105,43 @@ class entries extends \totara_userdata\userdata\item {
                  $join
                  WHERE e.userid = :userid
               ORDER BY e.id";
-        $entries = $DB->get_recordset_sql($sql, array('userid' => $user->id));
+        $entries = $DB->get_recordset_sql($sql, ['userid' => $user->id]);
         foreach ($entries as $entry) {
             if (!isset($glossaries[$entry->sourceglossaryid])) {
-                $glossaries[$entry->sourceglossaryid] = array('id' => $entry->sourceglossaryid, 'cmid' => $entry->cmid, 'cmidnumber' => $entry->cmidnumber, 'reset' => 0);
+                $glossaries[$entry->sourceglossaryid] = [
+                    'id' => $entry->sourceglossaryid,
+                    'cmid' => $entry->cmid,
+                    'cmidnumber' => $entry->cmidnumber,
+                    'reset' => 0,
+                ];
             }
-            $glossarycontext = \context_module::instance($entry->cmid);
+            $glossarycontext = context_module::instance($entry->cmid);
 
             // Delete ratings.
-            $rm->delete_ratings((object)array('contextid' => $glossarycontext->id, 'component' => 'mod_glossary', 'ratingarea' => 'entry', 'itemid' => $entry->id));
+            $rm->delete_ratings((object)[
+                'contextid' => $glossarycontext->id,
+                'component' => 'mod_glossary',
+                'ratingarea' => 'entry',
+                'itemid' => $entry->id
+            ]);
 
             // Delete comments.
-            \comment::delete_comments(array('contextid' => $glossarycontext->id, 'component' => 'mod_glossary', 'commentarea' => 'glossary_entry', 'itemid' => $entry->id));
+            comment::delete_comments([
+                'contextid' => $glossarycontext->id,
+                'component' =>'mod_glossary',
+                'commentarea' => 'glossary_entry',
+                'itemid' => $entry->id
+            ]);
+
+            // Delete attachment and inline (editor) files.
+            $fs->delete_area_files($glossarycontext->id, 'mod_glossary', 'attachment', $entry->id);
+            $fs->delete_area_files($glossarycontext->id, 'mod_glossary', 'entry', $entry->id);
 
             if ($entry->usedynalink and $entry->approved) {
                 $glossaries[$entry->sourceglossaryid]['reset'] = 1;
             }
             // Exported entries are a mess, better not trigger anything here.
-            $DB->set_field('glossary_entries', 'sourceglossaryid', 0, array('id' => $entry->id));
+            $DB->set_field('glossary_entries', 'sourceglossaryid', 0, ['id' => $entry->id]);
         }
         $entries->close();
 
@@ -122,23 +150,42 @@ class entries extends \totara_userdata\userdata\item {
                   FROM {glossary_entries} e
                  $join
                  WHERE e.userid = :userid
+                   AND e.sourceglossaryid = 0
               ORDER BY e.id";
-        $entries = $DB->get_recordset_sql($sql, array('userid' => $user->id));
+        $entries = $DB->get_recordset_sql($sql, ['userid' => $user->id]);
         foreach ($entries as $entry) {
             if (!isset($glossaries[$entry->glossaryid])) {
-                $glossaries[$entry->glossaryid] = array('id' => $entry->glossaryid, 'cmid' => $entry->cmid, 'cmidnumber' => $entry->cmidnumber, 'reset' => 0);
+                $glossaries[$entry->glossaryid] = [
+                    'id' => $entry->glossaryid,
+                    'cmid' => $entry->cmid,
+                    'cmidnumber' => $entry->cmidnumber,
+                    'reset' => 0
+                ];
             }
-            $glossarycontext = \context_module::instance($entry->cmid);
+            $glossarycontext = context_module::instance($entry->cmid);
 
             // Delete ratings.
-            $rm->delete_ratings((object)array('contextid' => $glossarycontext->id, 'component' => 'mod_glossary', 'ratingarea' => 'entry', 'itemid' => $entry->id));
+            $rm->delete_ratings((object)[
+                'contextid' => $glossarycontext->id,
+                'component' => 'mod_glossary',
+                'ratingarea' => 'entry',
+                'itemid' => $entry->id
+            ]);
 
             // Delete comments.
-            \comment::delete_comments(array('contextid' => $glossarycontext->id, 'component' => 'mod_glossary', 'commentarea' => 'glossary_entry', 'itemid' => $entry->id));
+            comment::delete_comments([
+                'contextid' => $glossarycontext->id,
+                'component' => 'mod_glossary',
+                'commentarea' => 'glossary_entry',
+                'itemid' => $entry->id
+            ]);
 
+            // Delete attachment and inline (editor) files.
             $fs->delete_area_files($glossarycontext->id, 'mod_glossary', 'attachment', $entry->id);
-            $DB->delete_records('glossary_alias', array('entryid' => $entry->id));
-            $DB->delete_records('glossary_entries', array('id' => $entry->id));
+            $fs->delete_area_files($glossarycontext->id, 'mod_glossary', 'entry', $entry->id);
+
+            $DB->delete_records('glossary_alias', ['entryid' => $entry->id]);
+            $DB->delete_records('glossary_entries', ['id' => $entry->id]);
 
             if ($entry->usedynalink and $entry->approved) {
                 $glossaries[$entry->glossaryid]['reset'] = 1;
@@ -147,27 +194,27 @@ class entries extends \totara_userdata\userdata\item {
             // Trigger event.
             unset($entry->cmid);
             unset($entry->cmidnumber);
-            $event = \mod_glossary\event\entry_deleted::create(array(
+            $event = entry_deleted::create([
                 'context' => $context,
                 'objectid' => $entry->id,
-                'other' => array(
+                'other' => [
                     'concept' => $entry->concept
-                )
-            ));
+                ]
+            ]);
             $event->add_record_snapshot('glossary_entries', $entry);
             $event->trigger();
         }
         $entries->close();
 
         foreach ($glossaries as $gdata) {
-            $glossary = $DB->get_record('glossary', array('id' => $gdata['id']));
+            $glossary = $DB->get_record('glossary', ['id' => $gdata['id']]);
             $glossary->cmidnumber = $gdata['cmidnumber'];
-            $course = $DB->get_record('course', array('id' => $glossary->course));
-            $cm = $DB->get_record('course_modules', array('id' => $gdata['cmid']));
+            $course = $DB->get_record('course', ['id' => $glossary->course]);
+            $cm = $DB->get_record('course_modules', ['id' => $gdata['cmid']]);
 
             // Purge concept cache if necessary.
             if ($gdata['reset']) {
-                \mod_glossary\local\concept_cache::reset_glossary($glossary);
+                concept_cache::reset_glossary($glossary);
             }
 
             // Delete cached RSS feeds.
@@ -175,9 +222,9 @@ class entries extends \totara_userdata\userdata\item {
                 glossary_rss_delete_file($glossary);
             }
 
-            if ($user->status !== $user::STATUS_DELETED) {
+            if ($user->status !== target_user::STATUS_DELETED) {
                 // Update completion state.
-                $completion = new \completion_info($course);
+                $completion = new completion_info($course);
                 if ($completion->is_enabled($cm) == COMPLETION_TRACKING_AUTOMATIC && $glossary->completionentries) {
                     $completion->update_state($cm, COMPLETION_INCOMPLETE, $user->id);
                 }
@@ -205,67 +252,60 @@ class entries extends \totara_userdata\userdata\item {
      * Export user data from this item.
      *
      * @param target_user $user
-     * @param \context $context restriction for exporting i.e., system context for everything and course context for course export
-     * @return \totara_userdata\userdata\export|int result object or integer error code self::RESULT_STATUS_ERROR or self::RESULT_STATUS_SKIPPED
+     * @param context $context restriction for exporting i.e., system context for everything and course context for course export
+     * @return export|int result object or integer error code self::RESULT_STATUS_ERROR or self::RESULT_STATUS_SKIPPED
      */
-    protected static function export(target_user $user, \context $context) {
+    protected static function export(target_user $user, context $context) {
         global $DB;
 
         $fs = get_file_storage();
 
-        $export = new \totara_userdata\userdata\export();
+        $export = new export();
 
         $join = self::get_activities_join($context, 'glossary', 'e.glossaryid', 'g');
         $sql = "SELECT e.*, cm.id AS cmid
                   FROM {glossary_entries} e
                  $join
                  WHERE e.userid = :userid
+                   AND e.sourceglossaryid = 0
               ORDER BY e.id";
-        $entries = $DB->get_recordset_sql($sql, array('userid' => $user->id));
+        $entries = $DB->get_recordset_sql($sql, ['userid' => $user->id]);
         foreach ($entries as $entry) {
-            $e = array('id' => $entry->id, 'glossaryid' => $entry->glossaryid, 'concept' => $entry->concept, 'definition' => $entry->definition);
-            $filecontext = \context_module::instance($entry->cmid, IGNORE_MISSING);
+            $e = ['id' => $entry->id, 'glossaryid' => $entry->glossaryid, 'concept' => $entry->concept, 'definition' => $entry->definition];
+            $filecontext = context_module::instance($entry->cmid, IGNORE_MISSING);
             if ($filecontext) {
-                $files = $fs->get_area_files($filecontext->id, 'mod_glossary', 'entry', $entry->id, "timemodified", false);
+                $files = $fs->get_area_files($filecontext->id, 'mod_glossary', 'entry', $entry->id, "timemodified", false, 0, $user->id);
                 if ($files) {
-                    $e['files'] = array();
+                    $e['files'] = [];
                     foreach ($files as $file) {
-                        if ($file->get_userid() != $user->id) {
-                            continue;
-                        }
-                        $e['files'][] = array('id' => $file->get_id(), 'filename' => $file->get_filepath() . $file->get_filename());
-                        $export->files[] = $file;
+                        $e['files'][] = $export->add_file($file);
                     }
                 }
                 if ($entry->attachment) {
-                    $files = $fs->get_area_files($filecontext->id, 'mod_glossary', 'attachment', $entry->id, "timemodified", false);
+                    $files = $fs->get_area_files($filecontext->id, 'mod_glossary', 'attachment', $entry->id, "timemodified", false, 0, $user->id);
                     if ($files) {
-                        $e['attachments'] = array();
+                        $e['attachments'] = [];
                         foreach ($files as $file) {
-                            if ($file->get_userid() != $user->id) {
-                                continue;
-                            }
-                            $e['attachments'][] = array('id' => $file->get_id(), 'filename' => $file->get_filepath() . $file->get_filename());
-                            $export->files[] = $file;
+                            $e['attachments'][] = $export->add_file($file);
                         }
                     }
                 }
             }
-            $export->data[] = $e;
+            $export->data[$entry->id] = $e;
         }
         $entries->close();
 
-        // There are no files in exported entries, also ignore any potential duplication of exported entries in export.
+        // Add export of attachment and inline (editor) files here when fixing bug TL-17394.
         $join = self::get_activities_join($context, 'glossary', 'e.sourceglossaryid', 'g');
         $sql = "SELECT e.*, cm.id AS cmid
                   FROM {glossary_entries} e
                  $join
                  WHERE e.userid = :userid
               ORDER BY e.id";
-        $entries = $DB->get_recordset_sql($sql, array('userid' => $user->id));
+        $entries = $DB->get_recordset_sql($sql, ['userid' => $user->id]);
         foreach ($entries as $entry) {
-            $e = array('id' => $entry->id, 'glossaryid' => $entry->glossaryid, 'concept' => $entry->concept, 'definition' => $entry->definition);
-            $export->data[] = $e;
+            $e = ['id' => $entry->id, 'glossaryid' => $entry->glossaryid, 'concept' => $entry->concept, 'definition' => $entry->definition];
+            $export->data[$entry->id] = $e;
         }
         $entries->close();
 
@@ -285,18 +325,19 @@ class entries extends \totara_userdata\userdata\item {
      * Count user data for this item.
      *
      * @param target_user $user
-     * @param \context $context restriction for counting i.e., system context for everything and course context for course data
+     * @param context $context restriction for counting i.e., system context for everything and course context for course data
      * @return int  integer is the count >= 0, negative number is error result self::RESULT_STATUS_ERROR or self::RESULT_STATUS_SKIPPED
      */
-    protected static function count(target_user $user, \context $context) {
+    protected static function count(target_user $user, context $context) {
         global $DB;
 
         $join = self::get_activities_join($context, 'glossary', 'e.glossaryid', 'g');
         $sql = "SELECT COUNT(e.id)
                   FROM {glossary_entries} e
                  $join
-                 WHERE e.userid = :userid";
-        $count = (int)$DB->count_records_sql($sql, array('userid' => $user->id));
+                 WHERE e.userid = :userid
+                   AND sourceglossaryid = 0";
+        $count = $DB->count_records_sql($sql, ['userid' => $user->id]);
 
         // Exported entries.
         $join = self::get_activities_join($context, 'glossary', 'e.sourceglossaryid', 'g');
@@ -304,6 +345,6 @@ class entries extends \totara_userdata\userdata\item {
                   FROM {glossary_entries} e
                  $join
                  WHERE e.userid = :userid";
-        return $count + (int)$DB->count_records_sql($sql, array('userid' => $user->id));
+        return $count + $DB->count_records_sql($sql, ['userid' => $user->id]);
     }
 }
