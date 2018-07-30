18ccf191405517526771e5ca91fcaa64da951a9f
Adrian Greeve <adrian@moodle.com>
2018-07-30 12:45:29 +1200

    TL-18148 / MDL-52100 filelib: Files that are oversized are checked with user.

--- lib/tests/filelib_test.php
+++ lib/tests/filelib_test.php
@@ -1051,7 +1051,6 @@ EOF;
     public static function create_draft_file($filedata = array()) {
         global $USER;
 
-        self::setAdminUser();
         $fs = get_file_storage();
 
         $filerecord = array(
@@ -1213,7 +1212,9 @@ EOF;
         global $USER;
 
         $this->resetAfterTest(true);
-        $this->setAdminUser();
+        // The admin has no restriction for max file uploads, so use a normal user.
+        $user = $this->getDataGenerator()->create_user();
+        $this->setUser($user);
         $fs = get_file_storage();
 
         $file = self::create_draft_file();
