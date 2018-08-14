877bff626feb32ca1e1332a70127b5f5ae062c9e
Brendan Cox <brendan.cox@totaralearning.com>
2018-08-14 15:50:29 +1200

    TL-18627 Moodle release 3.3.7

--- version.php
+++ version.php
@@ -29,11 +29,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2017051503.04;              // 20170515      = branching date YYYYMMDD - do not modify!
+$version  = 2017051507.00;              // 20170515      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 
-$release  = '3.3.1 (Build: 20170710)'; // Human-friendly version name
+$release  = '3.3.7 (Build: 20180709)'; // Human-friendly version name
 
 $branch   = '33';                       // This version's branch.
 $maturity = MATURITY_ALPHA;         // This version's maturity level.
