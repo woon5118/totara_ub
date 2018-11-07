71358ff4eaa96ca540c1378e3c2c85c366845b8a
Simon Coggins <simon.coggins@totaralearning.com>
2018-11-07 22:33:02 +1300

    TL-19432 Remove warning from content marketplace version files

--- totara/contentmarketplace/contentmarketplaces/goone/version.php
+++ totara/contentmarketplace/contentmarketplaces/goone/version.php
@@ -25,7 +25,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'contentmarketplace_goone';
 $plugin->release   = '2.0';
-// @todo Please don't bump version numbers without discussion until further notice!
 $plugin->version   = 2018101900;
 $plugin->requires  = 2015111606.00; // Totara 9+ is required.
 $plugin->maturity  = MATURITY_STABLE;
