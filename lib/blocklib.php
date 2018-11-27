3fb42ff2ea13ce44eb86b07c18f2fd8f913c6a06
Yuliya Bozhko <yuliya.bozhko@totaralearning.com>
2018-11-27 22:55:46 +0000

    TL-19457 blocks: Removed 'settings' block from blocks_add_default_system_blocks()

--- lib/blocklib.php
+++ lib/blocklib.php
@@ -2666,7 +2666,7 @@ function blocks_add_default_system_blocks() {
 
     $page = new moodle_page();
     $page->set_context(context_system::instance());
-    $page->blocks->add_blocks(array(BLOCK_POS_LEFT => array('settings')), '*', null, true); // Totara: we do want these!
+    // We don't add blocks required by the theme, they will be auto-created.
     // Totara: removed 'admin_bookmarks' block.
     $page->blocks->add_blocks(array(BLOCK_POS_RIGHT => array('totara_community')), 'site-index', null, null, 10); // Totara: add totara_community block to the frontpage.
 
