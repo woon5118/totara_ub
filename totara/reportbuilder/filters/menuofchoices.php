c26e3b91645d4c2ae285b3483b9a0c5c5c2bf584
Yuliya Bozhko <yuliya.bozhko@totaralearning.com>
2019-02-04 15:53:27 +0000

    TL-20113 totara_reportbuilder: Fixed menuofchoices filter
    Signed-off-by: Yuliya Bozhko <yuliya.bozhko@totaralearning.com>

--- totara/reportbuilder/filters/menuofchoices.php
+++ totara/reportbuilder/filters/menuofchoices.php
@@ -18,12 +18,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @author Oleg Demeshev <oleg.demeshev@totaralms.com>
- * @package totara
- * @subpackage reportbuilder
+ * @package totara_reportbuilder
  */
 
 /**
- * Menu of choices select filter based on a signle value.
+ * Menu of choices select filter based on a single value.
  */
 
 global $CFG;
@@ -33,35 +32,39 @@ require_once($CFG->dirroot . '/totara/reportbuilder/filters/select.php');
 class rb_filter_menuofchoices extends rb_filter_select {
 
     /**
+     * Constructor
+     *
+     * @param string  $type         The filter type (from the db or embedded source)
+     * @param string  $value        The filter value (from the db or embedded source)
+     * @param integer $advanced     If the filter should be shown by default (0) or only
+     *                              when advanced options are shown (1)
+     * @param integer $region       Which region this filter appears in.
+     * @param reportbuilder object $report The report this filter is for
+     * @param array   $defaultvalue Default value for the filter
+     */
+    public function __construct($type, $value, $advanced, $region, $report, $defaultvalue) {
+        parent::__construct($type, $value, $advanced, $region, $report, $defaultvalue);
+
+        // Always simple mode to ensure single value select.
+        $this->options['simplemode'] = true;
+    }
+
+    /**
      * Returns the condition to be used with SQL where
      * @param array $data filter settings
      * @return array containing filtering condition SQL clause and params
      */
     function get_sql_filter($data) {
-
         $value = $data['value'];
         $query = $this->get_field();
-        $simplemode = $this->options['simplemode'];
 
-        if ($simplemode) {
-            // Use "equal to" operator for simple select.
-            $operator = 1;
-        } else {
-            $operator = $data['operator'];
+        if ($value == '') {
+            // return 1=1 instead of TRUE for MSSQL support
+            return [' 1=1 ', []];
         }
 
-        if ($operator == 0) {
-            // Return 1=1 instead of TRUE for MSSQL support.
-            return array(' 1=1 ', array());
-        } else if ($operator == 1) {
-            // Equal to.
-            $param = rb_unique_param("fsequal_");
-            return array("{$query} = :{$param}", array($param => $value));
-        } else {
-            // Not equal to.
-            // Check for null case for is not operator.
-            $param = rb_unique_param("fsequal_");
-            return array("({$query} != :{$param} OR ({$query}) IS NULL)", array($param => $value));
-        }
+        // If value is not empty, use "equal to" operator for simple select.
+        $param = rb_unique_param("fsequal_");
+        return ["{$query} = :{$param}", [$param => $value]];
     }
 }
