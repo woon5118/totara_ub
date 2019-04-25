6fe1d4dcf49dd048e8bef5f97424a363a8e44705
Stephen Vickers <stephen@spvsoftwareproducts.com>
2019-04-25 19:38:45 +0100

    TL-23645 / MDL-62599 mod_lti: Correct content-item return
    Update method used to verify the JWT version of content-items being returned by a tool.

--- mod/lti/locallib.php
+++ mod/lti/locallib.php
@@ -3141,7 +3141,7 @@ function lti_convert_to_jwt($parms, $endpoint, $oauthconsumerkey, $typeid, $nonc
  * @param int    $typeid
  * @param string $jwtparam   JWT parameter
  *
- * @return array  containing tool type and the array of parameters
+ * @return array  message parameters
  * @throws moodle_exception
  */
 function lti_convert_from_jwt($typeid, $jwtparam) {
@@ -3155,7 +3155,7 @@ function lti_convert_from_jwt($typeid, $jwtparam) {
         $ok = !is_null($claims) && !empty($claims['iss']);
     }
     if ($ok) {
-        $tool = lti_verify_jwt_signature($typeid, $claims['iss'], $jwtparam);
+        lti_verify_jwt_signature($typeid, $claims['iss'], $jwtparam);
         $params['oauth_consumer_key'] = $claims['iss'];
         foreach (lti_get_jwt_claim_mapping() as $key => $mapping) {
             $claim = LTI_JWT_CLAIM_PREFIX;
@@ -3220,7 +3220,7 @@ function lti_convert_from_jwt($typeid, $jwtparam) {
     if (isset($params['lti_message_type']) && array_key_exists($params['lti_message_type'], $messagetypemapping)) {
         $params['lti_message_type'] = $messagetypemapping[$params['lti_message_type']];
     }
-    return array($tool, $params);
+    return $params;
 }
 
 /**
