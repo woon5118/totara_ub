aeb51d86047408b07ccf6ceeee20b8bafb9fc5ed
Petr Skoda <petr.skoda@totaralearning.com>
2019-09-20 11:18:39 +1200

    TL-22359 accesslib: bump version to trigger core capability upgrades

--- version.php
+++ version.php
@@ -47,7 +47,7 @@ $maturity = MATURITY_ALPHA;         // This version's maturity level.
 $TOTARA = new stdClass();
 
 $TOTARA->version    = '13dev';          // Please keep as string.
-$TOTARA->build      = '20190919.01';    // Please keep as string.
+$TOTARA->build      = '20190920.00';    // Please keep as string.
 
 if ($maturity == MATURITY_EVERGREEN) {
     $TOTARA->release = "Evergreen (Build: {$TOTARA->build})";
