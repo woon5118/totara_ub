0c0ff816978e6204d9c7e3868570dcc2741c5ab7
Aleksandr Baishev <aleksandr.baishev@totaralearning.com>
2019-12-12 16:51:31 +1300

    TL-21751 core_orm: Replace cumbersome repository and relation docblocks with mixins

--- lib/classes/orm/entity/relations/relation.php
+++ lib/classes/orm/entity/relations/relation.php
@@ -27,101 +27,13 @@ use core\orm\entity\entity;
 use core\orm\entity\repository;
 use core\orm\collection;
 use coding_exception;
-use core\orm\lazy_collection;
-use core\orm\paginator;
 use core\orm\query\builder;
-use core\orm\query\builder_base;
-use core\orm\query\join;
 
 /**
  * Class relation
  * This class outlines a scaffolding for defining a relationship between entities
  *
- * This is an automatically generated docblock
- * Please do not edit it directly
- * See {@see core_orm_builder_proxied_docblock_testcase::test_relation_has_phpdoc()}
- *
- * @method $this select($what)
- * @method $this add_select($what)
- * @method $this reset_select()
- * @method $this select_raw(string $what, array $params = [])
- * @method $this add_select_raw(string $what, array $params = [])
- * @method $this where($attribute, $condition = null, $value = null, bool $or = false)
- * @method $this where_field($attribute, $condition = null, $raw_value = null, bool $or = false)
- * @method $this where_null($attribute, bool $or = false)
- * @method $this or_where_null($attribute)
- * @method $this where_not_null($attribute, bool $or = false)
- * @method $this or_where_not_null($attribute)
- * @method $this where_in($attribute, array $values, bool $or = false)
- * @method $this or_where_in($attribute, array $values)
- * @method $this where_not_in($attribute, array $values, bool $or = false)
- * @method $this or_where_not_in($attribute, array $values)
- * @method $this where_like($attribute, string $value = '', bool $or = false)
- * @method $this where_like_raw($attribute, string $value, bool $or = false)
- * @method $this where_like_starts_with($attribute, string $value, bool $or = false)
- * @method $this where_like_ends_with($attribute, string $value, bool $or = false)
- * @method $this where_exists($builder, bool $or = false)
- * @method $this where_not_exists($builder, bool $or = false)
- * @method $this or_where_field($attribute, $condition = null, $raw_value = null)
- * @method $this or_where($attribute, $condition = null, $value = null)
- * @method $this or_where_like($attribute, string $value = '')
- * @method $this or_where_like_raw($attribute, string $value)
- * @method $this or_where_like_starts_with($attribute, string $value)
- * @method $this or_where_like_ends_with($attribute, string $value)
- * @method $this or_where_exists(\core\orm\query\builder $builder)
- * @method $this or_where_not_exists(\core\orm\query\builder $builder)
- * @method $this where_raw(string $sql, array $params = [], bool $or = false)
- * @method $this or_where_raw(string $sql, array $params = [])
- * @method $this nested_where(\Closure $closure, bool $or = false)
- * @method $this join($table, $source = null, $condition = null, $target = null, string $type = 'inner')
- * @method $this left_join($table, $source = null, $condition = null, $target = null)
- * @method $this right_join($table, $source = null, $condition = null, $target = null)
- * @method $this full_join($table, $source = null, $condition = null, $target = null)
- * @method $this cross_join($table)
- * @method $this reset_union()
- * @method $this order_by($column, string $direction = 'asc')
- * @method $this order_by_raw($what)
- * @method $this reset_order_by()
- * @method bool has_order_by()
- * @method $this group_by($fields)
- * @method $this group_by_raw(string $field_sql)
- * @method $this reset_group_by()
- * @method $this having($attribute, $condition = null, $value = null, bool $or = false)
- * @method $this or_having($attribute, $condition = null, $value = null)
- * @method $this having_raw(string $sql, array $params = [], bool $or = false)
- * @method $this or_having_raw(string $sql, array $params = [])
- * @method $this limit(?int $limit)
- * @method $this offset(?int $offset)
- * @method $this when(bool $condition, callable $true, ?callable $false = null)
- * @method $this unless(bool $condition, callable $true, ?callable $false = null)
- * @method \core\orm\entity\entity|null find(int $id)
- * @method \core\orm\entity\entity find_or_fail(int $id)
- * @method \core\orm\entity\entity|null first(bool $strict = false)
- * @method \core\orm\entity\entity first_or_fail()
- * @method \core\orm\entity\entity|null one(bool $strict = false)
- * @method paginator paginate(int $page = 1, int $per_page = 0)
- * @method paginator load_more(int $page, int $per_page = 0)
- * @method collection get()
- * @method lazy_collection get_lazy()
- * @method int count()
- * @method $this update($attributes)
- * @method $this update_record($record)
- * @method $this delete()
- * @method bool exists()
- * @method bool does_not_exist()
- * @method array get_last_executed_queries()
- * @method array get_last_executed_query()
- * @method $this set_parent(\core\orm\query\builder $builder)
- * @method builder|builder_base get_parent(bool $topmost = false)
- * @method $this set_aggregation(bool $aggregation)
- * @method string get_table()
- * @method $this as(string $alias)
- * @method string get_alias()
- * @method bool is_alias_used()
- * @method string get_alias_sql()
- * @method bool has_conditions()
- * @method join|null get_join($table, ?string $alias = null)
- * @method bool has_join($table, ?string $alias = null)
+ * @mixin builder
  */
 abstract class relation {
 
