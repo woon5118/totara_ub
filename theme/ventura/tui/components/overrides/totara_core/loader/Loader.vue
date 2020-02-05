8197ce0ec6a971a171921e46e4304fe6db3aa2aa
Kevin Hottinger <kevin.hottinger@totaralearning.com>
2020-02-05 14:26:41 +1300

    TL-23819 totara_core: Add loading Vue component

---
+++ theme/ventura/tui/components/overrides/totara_core/loader/Loader.vue
@@ -0,0 +1,77 @@
+<!--
+  This file is part of Totara Learn
+
+  Copyright (C) 2019 onwards Totara Learning Solutions LTD
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  @author Kevin Hottinger <kevin.hottinger@totaralearning.com>
+  @package theme_ventura
+-->
+
+<style lang="scss">
+.tui-loader {
+  $loading-fullpage: #{&}--fullpage;
+
+  position: relative;
+
+  // Don't show nested loaders
+  &--active > * .tui-loader__overlay {
+    display: none;
+  }
+
+  // If no slot content (e.g. initial load)
+  &__empty {
+    min-height: var(--tui-gap-8);
+  }
+
+  &__overlay {
+    position: absolute;
+    top: 0;
+    /*rtl:ignore*/
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    #{$loading-fullpage} & {
+      position: fixed;
+      z-index: var(--tui-zindex-loading-page);
+    }
+
+    &-positioner {
+      position: absolute;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      height: 100%;
+      background: var(--tui-color-background);
+      opacity: 0.85;
+      content: '';
+    }
+  }
+
+  &__display {
+    position: sticky;
+    top: var(--tui-gap-6);
+    bottom: var(--tui-gap-6);
+    font-size: var(--tui-font-size-18);
+
+    .fa-spinner {
+      position: relative;
+      top: -1px;
+    }
+  }
+}
+</style>
---
+++ totara/samples/tui/components/samples/totara_core/loader/Loader.vue
@@ -0,0 +1,98 @@
+<!--
+  This file is part of Totara Learn
+
+  Copyright (C) 2019 onwards Totara Learning Solutions LTD
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  @author Kevin Hottinger <kevin.hottinger@totaralearning.com>
+  @package totara_samples
+-->
+
+<template>
+  <div class="tui-loader">
+    ...
+
+    <SamplesExample>
+      <h4>Wrapping Content</h4>
+      <Loader :loading="loading" :fullpage="fullpage">
+        <h1>content</h1>
+        ....
+      </Loader>
+
+      <h4>Without Content</h4>
+      <Loader :loading="loading" />
+    </SamplesExample>
+
+    <SamplesPropCtl>
+      <FormRow v-slot="{ labelId }" label="Loading">
+        <RadioGroup v-model="loading" :horizontal="true">
+          <Radio :value="true">True</Radio>
+          <Radio :value="false">False</Radio>
+        </RadioGroup>
+      </FormRow>
+
+      <FormRow v-slot="{ labelId }" label="Full page">
+        <RadioGroup v-model="fullpage" :horizontal="true">
+          <Radio :value="true">True</Radio>
+          <Radio :value="false">False</Radio>
+        </RadioGroup>
+      </FormRow>
+    </SamplesPropCtl>
+
+    <SamplesCode>
+      <template v-slot:template>{{ codeTemplate }}</template>
+      <template v-slot:script>{{ codeScript }}</template>
+    </SamplesCode>
+  </div>
+</template>
+
+<script>
+import Loader from 'totara_core/components/loader/Loader';
+import FormRow from 'totara_core/components/form/FormRow';
+import Radio from 'totara_core/components/form/Radio';
+import RadioGroup from 'totara_core/components/form/RadioGroup';
+import SamplesCode from 'totara_samples/components/sample_parts/misc/SamplesCode';
+import SamplesExample from 'totara_samples/components/sample_parts/misc/SamplesExample';
+import SamplesPropCtl from 'totara_samples/components/sample_parts/misc/SamplesPropCtl';
+
+export default {
+  components: {
+    Loader,
+    FormRow,
+    Radio,
+    RadioGroup,
+    SamplesCode,
+    SamplesExample,
+    SamplesPropCtl,
+  },
+
+  data() {
+    return {
+      fullpage: false,
+      loading: true,
+      codeTemplate: `<Loader :loading="true">
+  ...content...
+</Loader>`,
+      codeScript: `import Loader from 'totara_core/components/loader/Loader;
+
+export default {
+  components: {
+    Loader,
+  }
+}`,
+    };
+  },
+};
+</script>
