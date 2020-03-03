6de4219ec382a015796275d8bed6549a28547353
Kevin Hottinger <kevin.hottinger@totaralearning.com>
2020-03-03 22:11:43 +1300

    TL-24286 totara_core: Add lozenge component

---
+++ theme/ventura/tui/components/overrides/totara_core/lozenge/Lozenge.vue
@@ -0,0 +1,46 @@
+<!--
+  This file is part of Totara Learn
+
+  Copyright (C) 2019 onwards Totara Learning Solutions LTD
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  @author Kevin Hottinger <kevin.hottinger@totaralearning.com>
+  @package theme_ventura
+-->
+
+<style lang="scss">
+.tui-lozenge {
+  display: inline-flex;
+  padding: 1px var(--tui-gap-2);
+  color: var(--tui-color-neutral-1);
+  @include tui-font-heading-label-small();
+  font-weight: 500;
+  line-height: 1.2;
+  background: var(--tui-color-prompt-info);
+  border-radius: var(--tui-border-radius-curved);
+
+  &--alert {
+    background: var(--tui-color-prompt-alert);
+  }
+
+  &--success {
+    background: var(--tui-color-prompt-success);
+  }
+
+  &--warning {
+    background: var(--tui-color-prompt-warning);
+  }
+}
+</style>
---
+++ totara/samples/tui/components/samples/totara_core/lozenge/Lozenge.vue
@@ -0,0 +1,90 @@
+<!--
+  This file is part of Totara Learn
+
+  Copyright (C) 2019 onwards Totara Learning Solutions LTD
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  @author Kevin Hottinger <kevin.hottinger@totaralearning.com>
+  @package totara_samples
+-->
+
+<template>
+  <div class="tui-loader">
+    A visual UI component
+
+    <SamplesExample>
+      <Lozenge :text="text" :type="type" />
+    </SamplesExample>
+
+    <SamplesPropCtl>
+      <FormRow v-slot="{ labelId }" label="Type">
+        <RadioGroup v-model="type" :horizontal="true">
+          <Radio value="alert">Alert</Radio>
+          <Radio value="info">Info</Radio>
+          <Radio value="success">Success</Radio>
+          <Radio value="warning">Warning</Radio>
+        </RadioGroup>
+      </FormRow>
+
+      <FormRow v-slot="{ id, label }" label="Text">
+        <InputText :id="id" v-model="text" :placeholder="text" />
+      </FormRow>
+    </SamplesPropCtl>
+
+    <SamplesCode>
+      <template v-slot:template>{{ codeTemplate }}</template>
+      <template v-slot:script>{{ codeScript }}</template>
+    </SamplesCode>
+  </div>
+</template>
+
+<script>
+import FormRow from 'totara_core/components/form/FormRow';
+import InputText from 'totara_core/components/form/InputText';
+import Lozenge from 'totara_core/components/lozenge/Lozenge';
+import Radio from 'totara_core/components/form/Radio';
+import RadioGroup from 'totara_core/components/form/RadioGroup';
+import SamplesCode from 'totara_samples/components/sample_parts/misc/SamplesCode';
+import SamplesExample from 'totara_samples/components/sample_parts/misc/SamplesExample';
+import SamplesPropCtl from 'totara_samples/components/sample_parts/misc/SamplesPropCtl';
+
+export default {
+  components: {
+    FormRow,
+    InputText,
+    Lozenge,
+    Radio,
+    RadioGroup,
+    SamplesCode,
+    SamplesExample,
+    SamplesPropCtl,
+  },
+
+  data() {
+    return {
+      text: 'Danger',
+      type: 'alert',
+      codeTemplate: `<Lozenge :text="text" :type="type" />`,
+      codeScript: `import Lozenge from 'totara_core/components/lozenge/Lozenge';
+
+export default {
+  components: {
+    Lozenge,
+  }
+}`,
+    };
+  },
+};
+</script>
