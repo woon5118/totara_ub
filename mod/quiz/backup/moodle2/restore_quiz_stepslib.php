e21cf0a8ceaada179389213a5da229c5b1fb8bde
Yuliya Bozhko <yuliya.bozhko@totaralearning.com>
2020-03-30 09:49:26 -0700

    TL-23459 mod_quiz: Adjust 'completionpass' setting after old restores
    Signed-off-by: Yuliya Bozhko <yuliya.bozhko@totaralearning.com>

--- mod/quiz/backup/moodle2/restore_quiz_stepslib.php
+++ mod/quiz/backup/moodle2/restore_quiz_stepslib.php
@@ -435,4 +435,31 @@ class restore_quiz_activity_structure_step extends restore_questions_activity_st
                     'shufflequestions' => $this->legacyshufflequestionsoption));
         }
     }
+
+    protected function after_restore() {
+        global $DB;
+
+        // Totara: fix quiz gradepass issue coming from Totara 2.7 and earlier.
+        if ($this->get_task()->get_info()->moodle_version < 2014051299) {
+            $params = [
+                'courseid'     => $this->get_courseid(),
+                'itemtype'     => 'mod',
+                'itemmodule'   => 'quiz',
+                'iteminstance' => $this->get_new_parentid('quiz'),
+            ];
+            $gradeitem = $DB->get_record('grade_items', $params);
+            $quiz = $DB->get_record('quiz', ['id' => $this->get_new_parentid('quiz')]);
+            $cm = get_coursemodule_from_instance('quiz', $quiz->id);
+            if ($cm->completion == 2) { // "Show activity as complete when conditions are met" option selected course.
+                if ($gradeitem->gradepass != 0) {
+                    $quiz->completionpass = '1';
+                    $DB->update_record('quiz', $quiz);
+                }
+            }
+            if ($quiz->completionpass == 1 && $cm->completiongradeitemnumber === null) {
+                $cm->completiongradeitemnumber = $gradeitem->itemnumber;
+                $DB->update_record('course_modules', $cm);
+            }
+        }
+    }
 }
