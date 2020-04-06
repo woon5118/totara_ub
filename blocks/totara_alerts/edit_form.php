e2bfe43123e5c1aec846fdd2d6ba8be70c4ce206
Yuliya Bozhko <yuliya.bozhko@totaralearning.com>
2020-04-06 08:57:56 -0700

    TL-17294 block_totara_alerts: Fix help text reference
    Signed-off-by: Yuliya Bozhko <yuliya.bozhko@totaralearning.com>

--- blocks/totara_alerts/edit_form.php
+++ blocks/totara_alerts/edit_form.php
@@ -39,7 +39,7 @@ class block_totara_alerts_edit_form extends block_edit_form {
         }
 
         $mform->addElement('select', 'config_showempty', get_string('showempty', 'block_totara_alerts'), $options, $attributes);
-        $mform->addHelpButton('config_showempty', 'showempty', 'block_totara_tasks');
+        $mform->addHelpButton('config_showempty', 'showempty', 'block_totara_alerts');
         $mform->setDefault('config_showempty', 0);
     }
 }
