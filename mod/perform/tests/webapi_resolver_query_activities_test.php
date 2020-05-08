6636311e0c563383fb14b567445adfd401ce0316
Fabian Derschatta <fabian.derschatta@totaralearning.com>
2020-05-08 12:22:05 +1200

    TL-24939 mod_perform: Add tests for can_manage and can_view_reporting fields on activities query

--- mod/perform/tests/webapi_resolver_query_activities_test.php
+++ mod/perform/tests/webapi_resolver_query_activities_test.php
@@ -27,8 +27,11 @@
 use core\webapi\execution_context;
 use mod_perform\webapi\resolver\query\activities;
 use totara_webapi\graphql;
+use totara_webapi\phpunit\webapi_phpunit_helper;
 
 class mod_perform_webapi_resolver_query_activities_testcase extends advanced_testcase {
+
+    use webapi_phpunit_helper;
 
     /**
      * Helper to get execution context
@@ -53,6 +56,86 @@ class mod_perform_webapi_resolver_query_activities_testcase extends advanced_tes
             ['Mid year performance', 'End year performance'],
             [$activities[0]->name, $activities[1]->name]
         );
+    }
+
+    public function test_user_needs_view_manage_activities_capability() {
+        global $DB;
+
+        $user1 = $this->getDataGenerator()->create_user();
+        $this->setUser($user1);
+        // $this->create_test_data();
+
+        $user_role = $DB->get_record('role', ['shortname' => 'user']);
+        assign_capability('mod/perform:view_manage_activities', CAP_ALLOW, $user_role->id, SYSCONTEXTID, true);
+
+        /** @var mod_perform\models\activity\activity[] $activities */
+        $activities = activities::resolve([], $this->get_execution_context());
+
+        // There are not activities but also no exceptions
+        $this->assertEmpty($activities);
+
+        unassign_capability('mod/perform:view_manage_activities', $user_role->id, SYSCONTEXTID);
+
+        $this->expectException(required_capability_exception::class);
+        $this->expectExceptionMessage('(Access the performance activities management interface)');
+
+        /** @var mod_perform\models\activity\activity[] $activities */
+        $this->resolve_graphql_query('mod_perform_activities');
+    }
+
+    public function test_users_can_view_reports_or_manage_other_users_activities() {
+        global $DB;
+
+        $user1 = $this->getDataGenerator()->create_user();
+        $user2 = $this->getDataGenerator()->create_user();
+        $this->setUser($user1);
+        $this->create_test_data();
+
+        $user_role = $DB->get_record('role', ['shortname' => 'user']);
+        assign_capability('mod/perform:view_manage_activities', CAP_ALLOW, $user_role->id, SYSCONTEXTID, true);
+
+        /** @var mod_perform\models\activity\activity[] $activities */
+        $activities = $this->resolve_graphql_query('mod_perform_activities');
+
+        $this->assertCount(2, $activities);
+        // Users who created activities can always manage and view reporting
+        foreach ($activities as $activity) {
+            $this->assertTrue($activity->can_manage());
+            $this->assertTrue($activity->can_view_participation_reporting());
+        }
+
+        $this->setUser($user2);
+
+        $activities = $this->resolve_graphql_query('mod_perform_activities');
+        $this->assertEmpty($activities);
+
+        // Let the other user manage only
+        assign_capability('mod/perform:manage_activity', CAP_ALLOW, $user_role->id, SYSCONTEXTID, true);
+
+        $activities = $this->resolve_graphql_query('mod_perform_activities');
+
+        foreach ($activities as $activity) {
+            $this->assertTrue($activity->can_manage());
+            $this->assertFalse($activity->can_view_participation_reporting());
+        }
+
+        // Let the user also see view the reports
+        assign_capability('mod/perform:view_participation_reporting', CAP_ALLOW, $user_role->id, SYSCONTEXTID, true);
+
+        $activities = $this->resolve_graphql_query('mod_perform_activities');
+
+        foreach ($activities as $activity) {
+            $this->assertTrue($activity->can_manage());
+            $this->assertTrue($activity->can_view_participation_reporting());
+        }
+
+        // Let the user just view reports
+        unassign_capability('mod/perform:manage_activity', $user_role->id, SYSCONTEXTID);
+
+        foreach ($activities as $activity) {
+            $this->assertFalse($activity->can_manage());
+            $this->assertTrue($activity->can_view_participation_reporting());
+        }
     }
 
     public function test_get_empty_activities() {
