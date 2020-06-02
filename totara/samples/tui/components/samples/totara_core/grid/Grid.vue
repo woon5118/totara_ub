e95b30048c36dd0d23141f726d9b95427e110c88
Dave Wallace <dave.wallace@totaralearning.com>
2020-06-02 13:35:00 +1200

    TL-23889 totara_samples: Improve Grid sample

---
+++ totara/samples/tui/components/samples/totara_core/grid/Grid.vue
@@ -0,0 +1,321 @@
+<!--
+  This file is part of Totara Learn
+
+  Copyright (C) 2019 onwards Totara Learning Solutions LTD
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  @author Dave Wallace <dave.wallace@totaralearning.com>
+  @package totara_samples
+-->
+
+<template>
+  <div class="tui-grid-example">
+    <SamplesExample>
+      <Grid
+        :direction="gridDirection"
+        :use-horizontal-gap="useHorizontalGap"
+        :use-vertical-gap="useVerticalGap"
+        :max-units="maxUnits"
+        :stack-at="stackAt"
+      >
+        <GridItem
+          v-for="(item, index) in gridItems"
+          :key="index"
+          :units="gridItems[index].units"
+          :grows="gridItems[index].grows"
+          :shrinks="gridItems[index].shrinks"
+          :overflows="gridItems[index].overflows"
+          :hyphens="gridItems[index].hyphens"
+        >
+          <Textarea
+            :ref="'textarea-' + index"
+            :placeholder="'GridItem content'"
+            @input="handleTextareaInput(index, $event)"
+          />
+
+          <Popover :triggers="['click']">
+            <template v-slot:trigger>
+              <ButtonIcon
+                :aria-label="'GridItem options'"
+                :caret="false"
+                :styleclass="{
+                  xsmall: true,
+                  transparent: true,
+                  transparentNoPadding: true,
+                }"
+              >
+                <EditIcon />
+              </ButtonIcon>
+            </template>
+
+            <p>Properties for this GridItem</p>
+            <Form>
+              <FormRow label="Units">
+                <InputNumber
+                  :placeholder="'(Integer)'"
+                  :min="1"
+                  :max="parseInt(maxUnits)"
+                  @input="handleChange(index, 'units', parseInt($event))"
+                />
+              </FormRow>
+              <FormRow label="Grows">
+                <RadioGroup
+                  v-model="gridItems[index].grows"
+                  :horizontal="true"
+                  @input="handleChange(index, 'grows', $event)"
+                >
+                  <Radio :value="true">True</Radio>
+                  <Radio :value="false">False (default)</Radio>
+                </RadioGroup>
+              </FormRow>
+              <FormRow label="Shrinks">
+                <RadioGroup
+                  v-model="gridItems[index].shrinks"
+                  :horizontal="true"
+                  @input="handleChange(index, 'shrinks', $event)"
+                >
+                  <Radio :value="false">False</Radio>
+                  <Radio :value="true">True (default)</Radio>
+                </RadioGroup>
+              </FormRow>
+              <FormRow label="Overflows">
+                <RadioGroup
+                  v-model="gridItems[index].overflows"
+                  :horizontal="true"
+                  @input="handleChange(index, 'overflows', $event)"
+                >
+                  <Radio :value="true">True</Radio>
+                  <Radio :value="false">False (default)</Radio>
+                </RadioGroup>
+              </FormRow>
+              <FormRow label="Hyphens">
+                <RadioGroup
+                  v-model="gridItems[index].hyphens"
+                  :horizontal="true"
+                  @input="handleChange(index, 'hyphens', $event)"
+                >
+                  <Radio :value="false">False</Radio>
+                  <Radio :value="true">True (default)</Radio>
+                </RadioGroup>
+              </FormRow>
+              <FormRow label="Order">
+                <em>Supported, but refer to implementation</em>
+              </FormRow>
+            </Form>
+          </Popover>
+        </GridItem>
+      </Grid>
+
+      <br />
+      <p>
+        <Button text="Add GridItem" @click="addGridItem" />
+        <Button text="Remove GridItem" @click="removeGridItem" />
+      </p>
+    </SamplesExample>
+
+    <SamplesPropCtl>
+      <FormRow label="Max GridItem units before wrapping">
+        <RadioGroup v-model="maxUnits" :horizontal="true">
+          <Radio :value="'12'">12 (default)</Radio>
+          <Radio :value="'16'">16</Radio>
+        </RadioGroup>
+      </FormRow>
+      <FormRow label="Direction">
+        <RadioGroup v-model="gridDirection" :horizontal="true">
+          <Radio :value="'horizontal'">Horizontal (default)</Radio>
+          <Radio :value="'vertical'">Vertical</Radio>
+        </RadioGroup>
+      </FormRow>
+      <FormRow label="Use a horizontal gap">
+        <RadioGroup v-model="useHorizontalGap" :horizontal="true">
+          <Radio :value="true">True (default)</Radio>
+          <Radio :value="false">False</Radio>
+        </RadioGroup>
+      </FormRow>
+      <FormRow label="Use a vertical gap">
+        <RadioGroup v-model="useVerticalGap" :horizontal="true">
+          <Radio :value="true">True (default)</Radio>
+          <Radio :value="false">False</Radio>
+        </RadioGroup>
+      </FormRow>
+      <FormRow label="Become stacked at width (px)">
+        <RadioGroup v-model="stackAt" :horizontal="true">
+          <Radio :value="0">0 (default)</Radio>
+          <Radio :value="400">400</Radio>
+        </RadioGroup>
+      </FormRow>
+    </SamplesPropCtl>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import Grid from 'totara_core/components/grid/Grid';
+import GridItem from 'totara_core/components/grid/GridItem';
+import SamplesExample from 'totara_samples/components/sample_parts/misc/SamplesExample';
+import SamplesPropCtl from 'totara_samples/components/sample_parts/misc/SamplesPropCtl';
+import Form from 'totara_core/components/form/Form';
+import FormRow from 'totara_core/components/form/FormRow';
+import InputNumber from 'totara_core/components/form/InputNumber';
+import Textarea from 'totara_core/components/form/Textarea';
+import Radio from 'totara_core/components/form/Radio';
+import RadioGroup from 'totara_core/components/form/RadioGroup';
+import Button from 'totara_core/components/buttons/Button';
+import ButtonIcon from 'totara_core/components/buttons/ButtonIcon';
+import EditIcon from 'totara_core/components/icons/common/Edit';
+import Popover from 'totara_core/components/popover/Popover';
+
+export default {
+  components: {
+    Grid,
+    GridItem,
+    SamplesExample,
+    SamplesPropCtl,
+    Form,
+    FormRow,
+    Radio,
+    RadioGroup,
+    InputNumber,
+    Textarea,
+    Button,
+    ButtonIcon,
+    EditIcon,
+    Popover,
+  },
+  data() {
+    return {
+      /**
+       * Main Grid
+       **/
+      gridDirection: 'horizontal',
+      useHorizontalGap: true,
+      useVerticalGap: true,
+      maxUnits: '12',
+      stackAt: 0,
+      /**
+       * GridItems
+       **/
+      stubGridItem: {
+        units: 4,
+        grows: false,
+        shrinks: true,
+        overflows: false,
+        hyphens: true,
+      },
+      // Modified dynamically by add/remove buttons and also individual GridItem
+      // prop settings popover component
+      gridItems: [
+        {
+          units: 4,
+          grows: false,
+          shrinks: true,
+          overflows: false,
+          hyphens: true,
+        },
+        {
+          units: 4,
+          grows: false,
+          shrinks: true,
+          overflows: false,
+          hyphens: true,
+        },
+        {
+          units: 4,
+          grows: false,
+          shrinks: true,
+          overflows: false,
+          hyphens: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    /**
+     * Adjust the size of the GridItem Textarea component so that we can show
+     * the effects of `overflows` and `hyphens` props
+     **/
+    handleTextareaInput: function(index) {
+      // need to use `$refs[*].$el` instead of just `$refs[*]` as the ref
+      // returned from the latter refers to the Textarea Vue component, not the
+      // actual DOM element
+      let ref = this.$refs['textarea-' + index].$el;
+      ref.style.height = '1px';
+      ref.style.height = ref.scrollHeight + 'px';
+    },
+
+    /**
+     * Update a GridItem's :units property reactively
+     **/
+    handleChange: function(index, prop, eventValue) {
+      if (eventValue === null || typeof eventValue === 'undefined') {
+        return;
+      }
+
+      // re-create gridItems Array index to invoke reactivity
+      let newObj = this.createNewGridItem(this.gridItems[index]);
+      newObj[prop] = eventValue;
+      Vue.set(this.gridItems, index, newObj);
+    },
+
+    /**
+     * Adds a GridItem to the example Grid
+     **/
+    addGridItem: function() {
+      this.gridItems.push(this.createNewGridItem());
+    },
+
+    /**
+     * Removes a GridItem to the example Grid
+     **/
+    removeGridItem: function() {
+      if (!this.gridItems.length) {
+        return;
+      }
+      this.gridItems.pop();
+    },
+
+    /**
+     * Create a new GridItem object with stub values
+     **/
+    createNewGridItem: function(oldItem) {
+      let obj = {};
+      Object.assign(obj, oldItem || this.stubGridItem);
+      return obj;
+    },
+  },
+};
+</script>
+<style lang="scss">
+.tui-grid-example .tui-grid-item {
+  position: relative; /* parental control of popover trigger position */
+  background-color: var(--tui-color-neutral-4);
+
+  > div > .tui-iconBtn {
+    position: absolute;
+    top: 6px;
+    right: 15px;
+    z-index: 1;
+  }
+
+  textarea {
+    overflow: hidden;
+  }
+
+  .tui-popoverFrame {
+    width: 500px;
+    max-width: 500px;
+  }
+}
+</style>
