5d25d82970d27f3b13a4f3f9931b4631ac9fe399
Sam Hemelryk <sam.hemelryk@totaralearning.com>
2020-07-19 16:29:20 +1200

    TL-26330 Added root level version.php for CI/CD and auto-deployment

---
+++ version.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of Totara Enterprise Extensions.
+ *
+ * Copyright (C) 2020 onwards Totara Learning Solutions LTD
+ *
+ * Totara Enterprise Extensions is provided only to Totara
+ * Learning Solutions LTD’s customers and partners, pursuant to
+ * the terms and conditions of a separate agreement with Totara
+ * Learning Solutions LTD or its affiliate.
+ *
+ * If you do not have an agreement with Totara Learning Solutions
+ * LTD, you may not access, use, modify, or distribute this software.
+ * Please contact [licensing@totaralearning.com] for more information.
+ *
+ * @author Sam Hemelryk <sam.hemelryk@totaralearning.com>
+ * @package totara
+ */
+
+/**
+ * This is here for Totara CI/CD and automatic deployment tools.
+ * We intend to update our tools to not require this file in the future, at which point it will be removed.
+ */
+
+$TOTARA = new \stdClass;
+$TOTARA->version    = '13dev';          // Please keep as string.
