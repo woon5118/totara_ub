a00b571cdf77135d39ab14be5c04de7a46ae1d4c
Sam Hemelryk <sam.hemelryk@totaralearning.com>
2020-07-19 16:28:08 +1200

    TL-26330 updated Tui docblocks

--- server/totara/core/tui/scripts/tasks/lint.js
+++ server/totara/core/tui/scripts/tasks/lint.js
@@ -1,23 +1,19 @@
-/*
- * This file is part of Totara Learn
+/**
+ * This file is part of Totara Enterprise Extensions.
  *
- * Copyright (C) 2019 onwards Totara Learning Solutions LTD
+ * Copyright (C) 2020 onwards Totara Learning Solutions LTD
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
+ * Totara Enterprise Extensions is provided only to Totara
+ * Learning Solutions LTD’s customers and partners, pursuant to
+ * the terms and conditions of a separate agreement with Totara
+ * Learning Solutions LTD or its affiliate.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * If you do not have an agreement with Totara Learning Solutions
+ * LTD, you may not access, use, modify, or distribute this software.
+ * Please contact [licensing@totaralearning.com] for more information.
  *
  * @author Simon Chester <simon.chester@totaralearning.com>
- * @package totara_core
+ * @module totara_core
  */
 
 const lint = require('../lib/lint');
