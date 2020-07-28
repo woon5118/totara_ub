f4e00a06cdf649902a97c9674ad95f821655c19a
Fabian Derschatta <fabian.derschatta@totaralearning.com>
2020-07-28 12:42:39 +1200

    TL-26479 mod_perform: Fix overrides being in wrong folders

--- client/src/theme_ventura/pages/overrides/mod_perform/UserActivities.vue
+++ client/src/theme_ventura/pages/overrides/mod_perform/UserActivities.vue
@@ -1,11 +1,37 @@
 <style lang="scss">
 .tui-performUserActivities {
+  &__header-row {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: flex-end;
+    justify-content: space-between;
+  }
+
+  &__heading {
+    margin: 0;
+    padding-right: var(--tui-gap-8);
+  }
+
   &__action-buttons {
     margin-top: var(--tui-gap-4);
   }
 
   &__content {
     margin-top: var(--tui-gap-4);
+  }
+
+  @include tui-font-body();
+}
+
+@media (min-width: $tui-screen-xs) {
+  .tui-performUserActivities {
+    &__action-buttons {
+      margin-top: var(--tui-gap-4);
+    }
+
+    &__content {
+      margin-top: var(--tui-gap-4);
+    }
   }
 }
 </style>
