6791239f3743a2061f3f027d93435e63fb4bf7ce
Cody Finegan <cody.finegan@totaralearning.com>
2020-08-05 09:18:05 +1200

    TL-26348 core_container: Fixing broken redirect hook when a course is not provided

--- server/badges/view.php
+++ server/badges/view.php
@@ -36,8 +36,11 @@ $page       = optional_param('page', 0, PARAM_INT);
 require_login();
 
 // Totara: allows the plugins to redirect away from this page if the course is not a legacy course
-$hook = new \core_badges\hook\view($courseid);
-$hook->execute();
+if ($courseid) {
+    $hook = new \core_badges\hook\view($courseid);
+    $hook->execute();
+}
+
 if (empty($CFG->enablebadges)) {
     print_error('badgesdisabled', 'badges');
 }
