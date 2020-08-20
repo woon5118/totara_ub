e17f4b01264e6d1c8688b7f8446fc5918f14439a
Chris Snyder <chris.snyder@totaralearning.com>
2020-08-20 02:44:03 +0000

    TL-26842 totara_mobile: Removed development upgrade steps

--- server/totara/mobile/db/upgrade.php
+++ server/totara/mobile/db/upgrade.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * This file is part of Totara LMS
  *
  * Copyright (C) 2019 onwards Totara Learning Solutions LTD
@@ -31,58 +31,6 @@ function xmldb_totara_mobile_upgrade($oldversion) {
     global $DB;
 
     $dbman = $DB->get_manager();
-
-    if ($oldversion < 2019101100) {
-        // Define table.
-        $table = new xmldb_table('totara_mobile_tokens');
-        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
-        $table->add_field('loginsecret', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
-        $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
-
-        // Adding keys to table.
-        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
-
-        // Adding indexes to table.
-        $table->add_index('loginsecret', XMLDB_INDEX_UNIQUE, array('loginsecret'));
-
-        // Conditionally launch create table.
-        if (!$dbman->table_exists($table)) {
-            $dbman->create_table($table);
-        }
-
-        upgrade_plugin_savepoint(true, 2019101100, 'totara', 'mobile');
-    }
-
-    if ($oldversion < 2020042900) {
-        // Define table.
-        $table = new xmldb_table('totara_mobile_compatible_courses');
-        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
-        $table->add_field('courseid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
-
-        // Add keys to table.
-        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
-        $table->add_key('courseid', XMLDB_KEY_FOREIGN_UNIQUE, array('courseid'), 'course', 'id');
-
-        // Conditionally launch create table.
-        if (!$dbman->table_exists($table)) {
-            $dbman->create_table($table);
-        }
-
-        upgrade_plugin_savepoint(true, 2020042900, 'totara', 'mobile');
-    }
-
-    if ($oldversion < 2020072202) {
-        // Define table persistent_login to be created.
-        $table = new xmldb_table('totara_mobile_devices');
-        $field = new xmldb_field('fcmtoken', XMLDB_TYPE_CHAR, '255', null, null, null, null);
-
-        // Conditionally add token field.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
-
-        upgrade_plugin_savepoint(true, 2020072202, 'totara', 'mobile');
-    }
 
     return true;
 }
