6cf7bd3a4c35d5d42ee662b5746218a81906e6c0
Cody Finegan <cody.finegan@totaralearning.com>
2020-08-31 12:43:19 +1200

    TL-26984 totara_playlist: Fixed playlist resource count follows the multitenancy rules

--- server/totara/playlist/classes/repository/playlist_resource_repository.php
+++ server/totara/playlist/classes/repository/playlist_resource_repository.php
@@ -25,6 +25,8 @@ namespace totara_playlist\repository;
 use core\orm\entity\entity;
 use core\orm\entity\repository;
 use core\orm\query\builder;
+use totara_engage\access\access_manager;
+use totara_engage\entity\engage_resource;
 use totara_playlist\entity\playlist_resource;
 
 final class playlist_resource_repository extends repository {
@@ -42,11 +44,84 @@ final class playlist_resource_repository extends repository {
 
     /**
      * @param int $playlistid
+     * @param int|null $user_id
      * @return int
      */
-    public function get_total_of_resources(int $playlistid): int {
-        $builder = builder::table(static::get_table());
-        $builder->where('playlistid', $playlistid);
+    public function get_total_of_resources(int $playlistid, ?int $user_id = null): int {
+        global $USER, $DB, $CFG;
+
+        $builder = builder::table(static::get_table(), 'pr');
+        $builder->where('pr.playlistid', $playlistid);
+        $builder->join(['playlist', 'p'], 'p.id', 'pr.playlistid');
+
+        if (null === $user_id) {
+            $user_id = $USER->id;
+        }
+
+        // We need to filter on multi-tenancy as resources could cross lines
+        if (!empty($CFG->tenantsenabled) && !access_manager::can_manage_tenant_participants($user_id)) {
+            // Join on the engage_resources table as we have to filter against the owner of the resource
+            $builder->join([engage_resource::TABLE, 'er'], 'er.id', 'pr.resourceid');
+
+            // Multi tenancy is on, and user is not a site admin.
+            $tenant_id = $DB->get_field('user', 'tenantid', ['id' => $user_id]);
+            if (null !== $tenant_id) {
+                // User is a part of any tenant.
+                $builder->join(
+                    ['user', 'u'],
+                    function (builder $join) use ($CFG, $tenant_id): void {
+                        $join->where_field('er.userid', 'u.id');
+                        $join->where('u.deleted', 0);
+                        $join->where('u.suspended', 0);
+
+                        if (empty($CFG->tenantsisolated)) {
+                            // Isolation mode is off.
+                            $join->where_raw(
+                                '(u.tenantid = :tenant_id OR u.tenantid IS NULL)',
+                                ['tenant_id' => $tenant_id]
+                            );
+                        } else {
+                            $join->where('u.tenantid', $tenant_id);
+                        }
+                    }
+                );
+            } else {
+                $tenant_builder = builder::table('tenant', 't');
+                $tenant_builder->select('t.id AS tenant_id');
+                $tenant_builder->join(['cohort_members', 'cm'], 't.cohortid', 'cm.cohortid');
+                $tenant_builder->where('cm.userid', $user_id);
+
+                $collection = $tenant_builder->get();
+                $tenant_ids = $collection->pluck('tenant_id');
+
+                if (!empty($tenant_ids)) {
+                    [$in_sql, $parameters] = $DB->sql_in($tenant_ids);
+
+                    $builder->join(
+                        ['user', 'u'],
+                        function (builder $join) use ($in_sql, $parameters): void {
+                            $join->where_field('er.userid', 'u.id');
+                            $join->where_raw(
+                                "(u.tenantid {$in_sql} OR u.tenantid IS NULL)",
+                                $parameters
+                            );
+                        }
+                    );
+                } else {
+                    $builder->join(
+                        ['user', 'u'],
+                        function (builder $join): void {
+                            $join->where_field('p.userid', 'u.id');
+                            $join->where_null('u.tenantid');
+                        }
+                    );
+                }
+
+                // In either cases, we need to exclude deleted/suspended users.
+                $builder->where('u.deleted', 0);
+                $builder->where('u.suspended', 0);
+            }
+        }
 
         return $builder->count();
     }
