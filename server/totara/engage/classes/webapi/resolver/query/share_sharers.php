9695cad3b7c6992409f978c98a3cf30f35e9b7bf
Johannes Cilliers <johannes.cilliers@cilbit.com>
2020-09-21 16:39:13 +1200

    TL-27427 totara_engage: Add validations to sharers query

--- server/totara/engage/classes/webapi/resolver/query/share_sharers.php
+++ server/totara/engage/classes/webapi/resolver/query/share_sharers.php
@@ -28,8 +28,11 @@ use core\webapi\middleware\require_advanced_feature;
 use core\webapi\middleware\require_login;
 use core\webapi\query_resolver;
 use core\webapi\resolver\has_middleware;
+use totara_engage\access\access_manager;
+use totara_engage\access\accessible;
 use totara_engage\entity\share;
 use totara_engage\repository\share_repository;
+use totara_engage\share\provider as share_provider;
 
 /**
  * Resolver for querying sharers.
@@ -47,8 +50,33 @@ final class share_sharers implements query_resolver, has_middleware {
             $ec->set_relevant_context(\context_user::instance($USER->id));
         }
 
+        if (!isset($args['itemid'])) {
+            throw new \coding_exception('ItemID is a required field.');
+        }
+
+        if (!isset($args['component'])) {
+            throw new \coding_exception('Component is a required field.');
+        }
+
         $itemid = $args['itemid'];
         $component = $args['component'];
+
+        // First validate item and user access.
+        try {
+            $item = share_provider::create($component)->get_item_instance($itemid);
+        } catch (\dml_exception $ex) {
+            // Don't expose internal exceptions!
+            throw new \moodle_exception('error:permissiondenied', 'totara_engage', '', null, $ex->getMessage());
+        }
+
+        if ($item instanceof accessible) {
+            if (!access_manager::can_access($item, $USER->id)) {
+                throw new \moodle_exception('error:permissiondenied', 'totara_engage', '', null, 'Cannot access item ' . $item->get_id());
+            }
+        } else {
+            // Something that is shareable must be accessible. It is a one way binding.
+            throw new \moodle_exception('error:permissiondenied', 'totara_engage', '', null, get_class($item));
+        }
 
         /** @var share_repository $repo */
         $repo = share::repository();
