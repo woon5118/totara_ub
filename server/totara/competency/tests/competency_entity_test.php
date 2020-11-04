1f12dbe465d4da7839ee3480156c50f2ed129973
Murali Nair <murali.nair@totaralearning.com>
2020-11-05 12:59:09 +1300

    TL-28158 totara_competency: fixed todos in totara_competency_competency_entity_testcase

--- server/totara/competency/tests/competency_entity_test.php
+++ server/totara/competency/tests/competency_entity_test.php
@@ -22,6 +22,8 @@
  */
 
 use core\orm\entity\relations\has_many_through;
+use core\orm\query\builder;
+use totara_competency\entities\assignment;
 use totara_competency\entities\competency;
 use totara_competency\entities\competency_framework;
 use totara_competency\entities\scale;
@@ -30,33 +32,103 @@ use totara_competency\entities\scale;
  * @group totara_competency
  */
 class totara_competency_competency_entity_testcase extends \advanced_testcase {
-
     /**
-     * Create data for the competency to test
+     * Create data for the competency to test.
      *
-     * @throws coding_exception
+     * return array an array containing these elements in this order:
+     *        - the test competency
+     *        - [assignment name => assignment] for that competency
+     *        - a [customfield type, customfield title, customfield value] tuple
      */
     public function create_data() {
-        $data = [
-            'competencies' => [],
+        self::setAdminUser();
+
+        $generator = $this->generator();
+        $competency = $generator->create_competency();
+        $competency_id = $competency->id;
+
+        $assignments = $generator->assignment_generator();
+        $user = $assignments->create_user();
+        $user_assignment = $assignments->create_user_assignment(
+            $competency_id,
+            $user->id,
+            ['type' => assignment::TYPE_ADMIN]
+        );
+
+        $hierarchies = $this->hierarchy_generator();
+        $fw = $hierarchies->create_pos_frame(['fullname' => 'Framework 2']);
+        $pos = $hierarchies->create_pos(['frameworkid' => $fw->id, 'fullname' => 'Position 1']);
+        $pos_assignment = $assignments->create_position_assignment($competency_id, $pos->id);
+
+        $fw = $hierarchies->create_org_frame(['fullname' => 'Framework 3']);
+        $org = $hierarchies->create_org(['frameworkid' => $fw->id, 'fullname' => 'Organisation 1']);
+        $org_assignment = $assignments->create_organisation_assignment($competency_id, $org->id);
+
+        $cohort = $assignments->create_cohort();
+        $cohort_assignment = $assignments->create_cohort_assignment($competency_id, $cohort->id);
+
+        $type_idnumber = 'COMPTYPE';
+        $comp_type = $hierarchies->create_comp_type(['idnumber' => $type_idnumber]);
+
+        $customfield_type = 'menu';
+        $customfield_title = 'Custom Field Label';
+        $customfield_value = "ZZZ";
+        $customfield_values = ['AAA', $customfield_value, 'CCC'];
+        builder::table('comp_type_info_field')->insert([
+            'typeid' => $comp_type,
+            'shortname' => $customfield_type,
+            'datatype' => $customfield_type,
+            'sortorder' => 1,
+            'hidden' => 0,
+            'locked' => 0,
+            'required' => 0,
+            'forceunique' => 0,
+            'param1' => implode("\n", $customfield_values),
+            'fullname' => 'Custom Field Label'
+        ]);
+
+        $hierarchies->create_hierarchy_type_generic_menu([
+            'hierarchy' => 'competency',
+            'value' => implode(',', $customfield_values),
+            'typeidnumber' => $type_idnumber
+        ]);
+
+        customfield_save_data(
+            (object) [
+                'id' => $competency_id,
+                'typeid' => $comp_type,
+                "customfield_$customfield_type" => 1,
+            ],
+            'competency',
+            'comp_type'
+        );
+
+        return [
+            $competency,
+            [
+                fullname($user) => $user_assignment,
+                $pos->fullname => $pos_assignment,
+                $org->fullname => $org_assignment,
+                $cohort->name => $cohort_assignment
+            ],
+            [
+                'type' => $customfield_type,
+                'title' => $customfield_title,
+                'value' => $customfield_value
+            ]
         ];
-
-        $data['competencies'][] = $this->generator()->create_competency();
-
-        return $data;
     }
 
     public function test_competency_entity() {
-        $data = $this->create_data();
-
-        $this->assertEqualsCanonicalizing($data['competencies'][0]->to_array(), competency::repository()->find($data['competencies'][0]->id)->to_array());
+        [$competency, ] = $this->create_data();
+        $this->assertEqualsCanonicalizing($competency->to_array(), competency::repository()->find($competency->id)->to_array());
     }
 
     public function test_it_has_related_scale() {
-        $data = $this->create_data();
+        [$competency, ] = $this->create_data();
 
-        $competency_id = (int) $data['competencies'][0]->id;
-        $framework_id = (int) $data['competencies'][0]->frameworkid;
+        $competency_id = (int) $competency->id;
+        $framework_id = (int) $competency->frameworkid;
 
         $framework = new competency_framework($framework_id);
         $competency = new competency($competency_id);
@@ -98,11 +170,25 @@ class totara_competency_competency_entity_testcase extends \advanced_testcase {
     }
 
     public function test_it_has_custom_fields_attribute() {
-        $this->markTestSkipped('TODO TO RESOLVE IN TL-28158 add test for custom fields attribute');
+        [,, $customfield] = $this->create_data();
+        $competencies = competency::repository()->get();
+        $this->assertEquals(1, $competencies->count(), 'wrong retrieval count');
+
+        $actual_customfields = $competencies->first()->display_custom_fields;
+        $this->assertCount(1, $actual_customfields, 'wrong custom field count');
+
+        $actual_customfield = (array)$actual_customfields[0];
+        $this->assertEqualsCanonicalizing($customfield, $actual_customfield, 'wrong customfield');
     }
 
     public function test_it_can_preload_assigned_user_groups() {
-        $this->markTestSkipped('TODO TO RESOLVE IN TL-28158 add test for custom fields attribute');
+        [, $assignments] = $this->create_data();
+        $competencies = competency::repository()->get();
+        $this->assertEquals(1, $competencies->count(), 'wrong retrieval count');
+
+        $actual_assignments = $competencies->first()->assigned_user_groups;
+        $this->assertCount(count($assignments), $actual_assignments, 'wrong assignment count');
+        $this->assertEqualsCanonicalizing(array_keys($assignments), $actual_assignments, 'wrong user group names');
     }
 
     /**
@@ -124,5 +210,4 @@ class totara_competency_competency_entity_testcase extends \advanced_testcase {
     public function hierarchy_generator() {
         return $this->getDataGenerator()->get_plugin_generator('totara_hierarchy');
     }
-
 }
