c2bf2c7209f7147f32730922291311f785854233
Maria Torres <maria.torres@totaralearning.com>
2020-11-23 18:49:53 +1300

    TL-28648 admin: Fix bulk user actions not respecting language-specific name order

--- server/admin/user/lib.php
+++ server/admin/user/lib.php
@@ -31,7 +31,8 @@ function get_selection_data($ufiltering) {
     $scount = count($SESSION->bulk_users);
 
     $userlist = array('acount'=>$acount, 'scount'=>$scount, 'ausers'=>false, 'susers'=>false, 'total'=>$total);
-    $userlist['ausers'] = $DB->get_records_select_menu('user', $sqlwhere, $params, 'fullname', 'id,'.$DB->sql_fullname().' AS fullname', 0, MAX_BULK_USERS);
+    $allusernames = get_all_user_name_fields(true);
+    $ausers = $DB->get_records_select('user', $sqlwhere, $params, '', 'id,'.$allusernames, 0, MAX_BULK_USERS);
 
     if ($scount) {
         if ($scount < MAX_BULK_USERS) {
@@ -40,8 +41,38 @@ function get_selection_data($ufiltering) {
             $bulkusers = array_slice($SESSION->bulk_users, 0, MAX_BULK_USERS, true);
         }
         list($in, $inparams) = $DB->get_in_or_equal($bulkusers);
-        $userlist['susers'] = $DB->get_records_select_menu('user', "id $in", $inparams, 'fullname', 'id,'.$DB->sql_fullname().' AS fullname');
+        $susers = $DB->get_records_select('user', "id $in", $inparams, '', 'id,'.$allusernames);
+    }
+
+    // Loop through ausers list and sort it by fullname.
+    if (!empty($ausers)) {
+        $userlist['ausers'] = user_sorted_by_fullname($ausers);
+    }
+
+    // Loop through susers list and sort it by fullname.
+    if (!empty($susers)) {
+        $userlist['susers'] = user_sorted_by_fullname($susers);
     }
 
     return $userlist;
+}
+
+/**
+ * Given an array of user objects, returns an array of users sorted by fullname.
+ *
+ * @param $users Array of users. Needs to have user ID and all usernames to get the fullname
+ * @return array Array in the form of user ID as key and fullname as value, sorted by fullname.
+ */
+function user_sorted_by_fullname(array $users) {
+    $sorted_users = array();
+
+    // Get the user's fullname.
+    foreach ($users as $user) {
+        $sorted_users[$user->id] = fullname($user);
+    }
+
+    // Sort users by fullname keeping the user key.
+    asort($sorted_users);
+
+    return $sorted_users;
 }
