2a579bba54156c024b2a8c6e9828ff77c51b8206
Fabian Derschatta <fabian.derschatta@totaralearning.com>
2020-12-23 10:54:57 +1300

    TL-29121 totara_msteams: Fix behat issue

--- server/totara/msteams/tests/behat/behat_totara_msteams.php
+++ server/totara/msteams/tests/behat/behat_totara_msteams.php
@@ -106,7 +106,7 @@ class behat_totara_msteams extends behat_base {
      */
     public function i_log_out_microsoft_teams() {
         // Nothing we can do other than logging out of the current session.
-        $this->getSession()->visit($this->locate_path('login'));
+        $this->getSession()->visit($this->locate_path('login/index.php'));
         $this->wait_for_pending_js();
         $this->execute('behat_general::i_click_on', ['Log out', 'button']);
     }
