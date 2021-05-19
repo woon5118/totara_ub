5b374688966fb3a12ab451c4f375634aebf41bcb
Sam Hemelryk <sam.hemelryk@totaralearning.com>
2021-05-20 11:33:06 +1200

    TL-30929 version.php: Bumping version 13.8+

--- server/version.php
+++ server/version.php
@@ -46,7 +46,7 @@ $maturity = MATURITY_STABLE;             // This version's maturity level.
 
 $TOTARA = new stdClass();
 
-$TOTARA->version    = '13.8';     // Please keep as string.
+$TOTARA->version    = '13.8+';     // Please keep as string.
 $TOTARA->build      = '20210519.00';    // Please keep as string.
 
 $TOTARA->release = "{$TOTARA->version} (Build: {$TOTARA->build})";
